5f7837c97adbd9bc9398f3051270cb1deb0d6dac
Brandon Istenes <bistenes@gmail.com>
2020-06-13 01:29:25 -0700

    Add isUuid validator (#42)
    * Add isUuid
    
    * Fix CIEL external ID validation
    
    * Add bad CIEL External ID example

--- packages/esm-config/src/validators/validators.test.ts
+++ packages/esm-config/src/validators/validators.test.ts
@@ -1,4 +1,4 @@
-import { isString, isBoolean } from "./validators";
+import { isString, isBoolean, isUuid } from "./validators";
 
 describe("isString", () => {
   it("accepts strings", () => {
@@ -17,5 +17,27 @@ describe("isBoolean", () => {
 
   it("rejects non-bools", () => {
     expect(isBoolean(1)).toMatch("must be a bool");
+  });
+});
+
+describe("isUuid", () => {
+  it("accepts a CIEL External ID", () => {
+    expect(isUuid("123118AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")).toBeUndefined();
+  });
+
+  it("accepts a valid UUID with dashes", () => {
+    expect(isUuid("28c37ff6-0079-4fa7-b803-5d547ac454e0")).toBeUndefined();
+  });
+
+  it("rejects a bad UUID", () => {
+    expect(isUuid("28c37ff6-0079-4fa7-b803-5d547ac454e")).toMatch(
+      "must be a valid UUID"
+    );
+  });
+
+  it("rejects a bad CIEL External ID", () => {
+    expect(isUuid("123118AAAAAAAAAAAAAAAAAAAAAAAAAAAAA")).toMatch(
+      "must be a valid UUID"
+    );
   });
 });
--- packages/esm-config/src/validators/validators.ts
+++ packages/esm-config/src/validators/validators.ts
@@ -10,4 +10,12 @@ export const isBoolean = validator(
   "must be a boolean"
 );
 
+export const isUuid = validator(
+  val =>
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+      val
+    ) || /^[0-9a-f]{36}$/i.test(val),
+  "must be a valid UUID or a 36-character hexadecimal string"
+);
+
 export const validators = { isString, isBoolean };
