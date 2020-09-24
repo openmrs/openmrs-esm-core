7600e0dcde07cd8b66d64ee734293f85e9481286
Brandon Istenes <bistenes@gmail.com>
2020-09-24 10:10:37 -0700

    Fixup

--- packages/esm-extension-manager/src/setup-tests.js
+++ packages/esm-extension-manager/src/setup-tests.js
@@ -1,9 +1,5 @@
 window.System = {
   import: (name) => import(name),
-  resolve: jest.fn().mockImplementation(() => {
-    throw new Error("config.json not available in import map");
-  }),
-  register: jest.fn(),
 };
 
 window.openmrsBase = "/openmrs";
