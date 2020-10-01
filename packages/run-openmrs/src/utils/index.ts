919482918f47ebd598d49bb5332cf3e3e0266d16
Florian Rappl <florian.rappl@smapiot.com>
2020-10-01 22:33:12 +0200

    Warn if importmap seems wrong

--- packages/run-openmrs/src/utils/index.ts
+++ packages/run-openmrs/src/utils/index.ts
@@ -54,9 +54,16 @@ export function getImportmap(value: string): ImportmapDeclaration {
     const path = resolve(process.cwd(), value);
 
     if (existsSync(path)) {
+      const content = readFileSync(path, "utf8");
+      const valid = checkImportmapJson(content);
+
+      if (!valid) {
+        console.warn(`The importmap provided in "${value}" does not seem right. Skipping.`);
+      }
+
       return {
         type: "inline",
-        value: readFileSync(path, "utf8"),
+        value: valid ? content : '',
       };
     } else if (checkImportmapJson(value)) {
       return {
