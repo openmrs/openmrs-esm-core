7a996eb589fea497fe5eae7be25c9c38c67eff66
Florian Rappl <florian.rappl@smapiot.com>
2020-10-07 14:06:56 +0200

    Fixed

--- packages/openmrs/src/utils/untar.ts
+++ packages/openmrs/src/utils/untar.ts
@@ -1,6 +1,6 @@
-import * as tar from 'tar';
-import { createGunzip } from 'zlib';
-import { EventEmitter } from 'events';
+import * as tar from "tar";
+import { createGunzip } from "zlib";
+import { EventEmitter } from "events";
 
 const TarParser = tar.Parse as any;
 
@@ -18,19 +18,19 @@ export function untar(stream: NodeJS.ReadableStream): Promise<PackageFiles> {
   return new Promise((resolve, reject) => {
     const files: PackageFiles = {};
     stream
-      .on('error', reject)
+      .on("error", reject)
       .pipe(createGunzip())
-      .on('error', reject)
+      .on("error", reject)
       .pipe(new TarParser())
-      .on('error', reject)
-      .on('entry', (e: ReadEntry) => {
+      .on("error", reject)
+      .on("entry", (e: ReadEntry) => {
         const content: Array<Buffer> = [];
         const p = e.path;
 
-        e.on('error', reject);
-        e.on('data', (c: Buffer) => content.push(c));
-        e.on('end', () => (files[p] = Buffer.concat(content)));
+        e.on("error", reject);
+        e.on("data", (c: Buffer) => content.push(c));
+        e.on("end", () => (files[p] = Buffer.concat(content)));
       })
-      .on('end', () => resolve(files));
+      .on("end", () => resolve(files));
   });
 }
