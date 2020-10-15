1f3994452e31ea05cf9c8ea5758d7ed65ff42295
Brandon Istenes <bistenes@gmail.com>
2020-10-15 17:33:53 -0500

    Fix esm-config exports

--- packages/esm-config/src/index.ts
+++ packages/esm-config/src/index.ts
@@ -1,7 +1,11 @@
 export * from "./module-config/module-config";
-export * from "./react-hook/use-config";
+export { useConfig } from "./react-hook/use-config";
+export { useExtensionConfig } from "./react-hook/use-extension-config";
 export * from "./navigation/navigate";
 export * from "./navigation/react-configurable-link";
 export * from "./navigation/interpolate-string";
 export * from "./validators/validator";
 export * from "./validators/validators";
+
+// compatibility hack for old versions of react-root-decorator
+export { ModuleNameContext } from "@openmrs/esm-core-context";
