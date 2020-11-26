598293be598f1ad100678517847d1b2a706ec949
Brandon Istenes <bistenes@gmail.com>
2020-11-26 15:15:32 -0800

    Mock esm-api store

--- packages/esm-implementer-tools-app/__mocks__/openmrs-esm-api.mock.tsx
+++ packages/esm-implementer-tools-app/__mocks__/openmrs-esm-api.mock.tsx
@@ -1,3 +1,20 @@
 export function openmrsFetch() {
   return new Promise(() => {});
 }
+
+let state;
+
+export const createGlobalStore = jest.fn().mockImplementation((n, value) => {
+  state = value;
+});
+
+export const getGlobalStore = jest.fn().mockImplementation(() => ({
+  getState: () => state,
+  setState: (val) => {
+    state = { ...state, ...val };
+  },
+  subscribe: (updateFcn) => {
+    updateFcn(state);
+    return () => {};
+  },
+}));
