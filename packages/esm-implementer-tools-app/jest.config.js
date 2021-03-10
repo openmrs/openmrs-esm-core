15fc757ca06e651193ba0f32bda24e83c0061dbf
samuelmale <samuelsmalek@gmail.com>
2021-03-10 11:32:36 +0300

    Add base files

--- packages/esm-implementer-tools-app/jest.config.js
+++ packages/esm-implementer-tools-app/jest.config.js
@@ -11,6 +11,6 @@ module.exports = {
     "^@carbon/icons-react/es/(.*)$": "@carbon/icons-react/lib/$1",
     "^carbon-components-react/es/(.*)$": "carbon-components-react/lib/$1",
     "@openmrs/esm-framework":
-      "<rootDir>/__mocks__/openmrs-esm-framework.mock.tsx",
+      "@openmrs/esm-framework/mocks/openmrs-esm-framework.mock.tsx",
   },
 };
