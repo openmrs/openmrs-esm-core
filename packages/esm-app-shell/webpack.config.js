7a2d3ab9cac8e66da86a31257c023596d2635d2c
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-03-26 17:21:33 +0100

    Explicitly include the importmap URL in the precached app shell files in order to also have external URLs in place.

--- packages/esm-app-shell/webpack.config.js
+++ packages/esm-app-shell/webpack.config.js
@@ -162,6 +162,9 @@ module.exports = (env, argv = {}) => {
         swSrc: "./src/service-worker.ts",
         maximumFileSizeToCacheInBytes:
           mode === "production" ? undefined : Number.MAX_SAFE_INTEGER,
+        additionalManifestEntries: [
+          { url: openmrsImportmapUrl, revision: null },
+        ],
       }),
     ],
   };
