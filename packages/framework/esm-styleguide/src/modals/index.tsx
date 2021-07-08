550bd9f22f98bc822e94ecee3ed7917015f5b734
Nikita Malyschkin <nikita.malyschkin@googlemail.com>
2021-07-08 18:26:23 +0200

    add modal chrome

--- packages/framework/esm-styleguide/src/modals/index.tsx
+++ packages/framework/esm-styleguide/src/modals/index.tsx
@@ -20,6 +20,33 @@ const modalStore = createGlobalStore<ModalState>("globalModalState", {
   modalStack: [],
 });
 
+function htmlToElement(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstChild as ChildNode;
+}
+
+function createModalFrame() {
+  const closeButton = htmlToElement(
+    `
+  <button
+    class="bx--modal-close"
+    type="button">
+    <svg id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style>.cls-1{fill:#000000;}.cls-2{fill:none;}</style></defs><title>close</title><polygon class="cls-1" points="24 9.4 22.6 8 16 14.6 9.4 8 8 9.4 14.6 16 8 22.6 9.4 24 16 17.4 22.6 24 24 22.6 17.4 16 24 9.4"/><rect class="cls-2" width="32" height="32"/></svg>
+  </button>`.trim()
+  ) as HTMLButtonElement;
+
+  closeButton.addEventListener("click", () => closeHighestInstance());
+  const outer = document.createElement("div");
+  outer.className = "bx--modal-container";
+  const contentContainer = document.createElement("div");
+
+  outer.append(closeButton);
+  outer.append(contentContainer);
+
+  return { outer, contentContainer };
+}
+
 function handleModalStateUpdate({ modalStack, modalContainer }: ModalState) {
   if (!modalContainer) return;
 
@@ -33,9 +60,10 @@ function handleModalStateUpdate({ modalStack, modalContainer }: ModalState) {
     modalStack.forEach((instance, index) => {
       switch (instance.state) {
         case "NEW":
-          instance.container = document.createElement("div");
+          const { outer, contentContainer } = createModalFrame();
+          instance.container = outer;
           instance.cleanup = renderExtension(
-            instance.container,
+            contentContainer,
             "",
             "",
             instance.extensionId,
@@ -82,15 +110,19 @@ export function renderModals(modalContainer: HTMLElement | null) {
   }
 }
 
+function closeHighestInstance() {
+  const state = modalStore.getState();
+  modalStore.setState({
+    ...state,
+    modalStack: state.modalStack.map((instance, i) =>
+      i === 0 ? { ...instance, state: "TO_BE_DELETED" } : instance
+    ),
+  });
+}
+
 function handleEscKey(e: KeyboardEvent) {
   if (e.key === "Escape") {
-    const state = modalStore.getState();
-    modalStore.setState({
-      ...state,
-      modalStack: state.modalStack.map((instance, i) =>
-        i === 0 ? { ...instance, state: "TO_BE_DELETED" } : instance
-      ),
-    });
+    closeHighestInstance();
   }
 }
 
