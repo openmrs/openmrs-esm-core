81ff120bcb801c1a8257afa4ada8e358f99f8aea
Amos Laboso <amos.laboso@gmail.com>
2021-07-22 14:50:40 +0300

    Changed strict backend dependency webservices.rest from 2.24.0 to ^2.24.0 and appui from 1.10.0 to ^1.10.0 (#153)
    Co-authored-by: amuj <alaboso@uonbi.ac.ke>

--- packages/apps/esm-login-app/src/index.ts
+++ packages/apps/esm-login-app/src/index.ts
@@ -9,8 +9,8 @@ const importTranslation = require.context(
 );
 
 const backendDependencies = {
-  "webservices.rest": "2.24.0",
-  appui: "1.10.0",
+  "webservices.rest": "^2.24.0",
+  appui: "^1.10.0",
 };
 
 const frontendDependencies = {
