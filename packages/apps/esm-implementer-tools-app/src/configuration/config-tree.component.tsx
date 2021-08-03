923a898e453fe7ae92a771e37ab7ab45aff5e896
Brandon Istenes <bistenes@gmail.com>
2021-08-03 13:29:12 -0700

    MF-533: Add a search bar to Implementer Tools

--- packages/apps/esm-implementer-tools-app/src/configuration/config-tree.component.tsx
+++ packages/apps/esm-implementer-tools-app/src/configuration/config-tree.component.tsx
@@ -4,21 +4,12 @@ import Accordion, {
   AccordionItem,
 } from "carbon-components-react/es/components/Accordion";
 import { ConfigTreeForModule } from "./config-tree-for-module.component";
-import { ExtensionStore, useExtensionStore } from "@openmrs/esm-framework";
-
-function hasSlot(store: ExtensionStore, moduleName: string) {
-  return Object.values(store.slots).some((slot) =>
-    Object.keys(slot.instances).includes(moduleName)
-  );
-}
 
 export interface ConfigTreeProps {
   config: Record<string, any>;
 }
 
 export function ConfigTree({ config }: ConfigTreeProps) {
-  const store = useExtensionStore();
-
   return (
     <div>
       <Accordion align="start">
@@ -27,8 +18,7 @@ export function ConfigTree({ config }: ConfigTreeProps) {
             .sort()
             .map((moduleName) => {
               const moduleConfig = config[moduleName];
-              return Object.keys(moduleConfig).length ||
-                hasSlot(store, moduleName) ? (
+              return Object.keys(moduleConfig).length ? (
                 <AccordionItem
                   title={<h6 className={styles.moduleName}>{moduleName}</h6>}
                   className={styles.fullWidthAccordion}
--- packages/apps/esm-implementer-tools-app/src/configuration/configuration.component.tsx
+++ packages/apps/esm-implementer-tools-app/src/configuration/configuration.component.tsx
@@ -1,23 +1,36 @@
-import React from "react";
-import TrashCan16 from "@carbon/icons-react/es/trash-can/16";
-import Download16 from "@carbon/icons-react/es/download/16";
+import React, { useMemo, useState } from "react";
 import ChevronDown16 from "@carbon/icons-react/es/chevron--down/16";
 import ChevronUp16 from "@carbon/icons-react/es/chevron--up/16";
-import Button from "carbon-components-react/es/components/Button";
-import Toggle from "carbon-components-react/es/components/Toggle";
-import styles from "./configuration.styles.css";
+import Download16 from "@carbon/icons-react/es/download/16";
+import TrashCan16 from "@carbon/icons-react/es/trash-can/16";
 import {
+  Config,
   ConfigInternalStore,
   configInternalStore,
   implementerToolsConfigStore,
   temporaryConfigStore,
+  useExtensionStore,
   useStore,
 } from "@openmrs/esm-framework";
+import { TextInput } from "carbon-components-react";
+import Button from "carbon-components-react/es/components/Button";
 import { Column, Grid, Row } from "carbon-components-react/es/components/Grid";
-import { ConfigTree } from "./config-tree.component";
-import { Description } from "./description.component";
-import { implementerToolsStore, ImplementerToolsStore } from "../store";
+import Toggle from "carbon-components-react/es/components/Toggle";
+import cloneDeep from "lodash-es/cloneDeep";
+import isEmpty from "lodash-es/isEmpty";
 import { useTranslation } from "react-i18next";
+import { implementerToolsStore, ImplementerToolsStore } from "../store";
+import { ConfigTree } from "./config-tree.component";
+import styles from "./configuration.styles.css";
+import { Description } from "./description.component";
+
+function isLeaf(configNode: Config) {
+  return (
+    typeof configNode == "object" &&
+    !Array.isArray(configNode) &&
+    (configNode["_default"] || configNode["_type"])
+  );
+}
 
 const actions = {
   toggleIsUIEditorEnabled({ isUIEditorEnabled }: ImplementerToolsStore) {
@@ -68,7 +81,9 @@ export const Configuration: React.FC<ConfigurationProps> = () => {
     configActions
   );
   const { config } = useStore(implementerToolsConfigStore);
+  const extensionStore = useExtensionStore();
   const tempConfigStore = useStore(temporaryConfigStore);
+  const [filterText, setFilterText] = useState("");
   const tempConfig = tempConfigStore.config;
   const tempConfigObjUrl = new Blob(
     [JSON.stringify(tempConfig, undefined, 2)],
@@ -77,12 +92,56 @@ export const Configuration: React.FC<ConfigurationProps> = () => {
     }
   );
 
+  const combinedConfig = useMemo(() => {
+    const result = cloneDeep(config);
+    for (let slot of Object.values(extensionStore.slots)) {
+      for (let moduleName of Object.keys(slot.instances)) {
+        if (!result[moduleName].extensions) {
+          result[moduleName].extensions = {};
+        }
+        if (!result[moduleName].extensions[slot.name]) {
+          result[moduleName].extensions[slot.name] = {};
+        }
+      }
+    }
+    return result;
+  }, [config, extensionStore]);
+
+  const filteredConfig = useMemo(() => {
+    function getRelatedBranches(inputTree: Config, filterText: string) {
+      const result = {};
+      for (let k of Object.keys(inputTree)) {
+        if (k.includes(filterText)) {
+          result[k] = cloneDeep(inputTree[k]);
+        } else {
+          if (!isLeaf(inputTree[k])) {
+            const kSubtreeResult = getRelatedBranches(inputTree[k], filterText);
+            if (!isEmpty(kSubtreeResult)) {
+              result[k] = kSubtreeResult;
+            }
+          }
+        }
+      }
+      return result;
+    }
+    return filterText
+      ? getRelatedBranches(combinedConfig, filterText)
+      : combinedConfig;
+  }, [filterText, combinedConfig]);
+
   return (
     <>
       <div className={styles.tools}>
         {isConfigToolbarOpen ? (
           <Grid style={{ margin: "0.25rem", padding: "0.5em 1.5em" }}>
             <Row>
+              <Column sm={1} md={2}>
+                <TextInput
+                  id="extensionSearch"
+                  labelText="Search extensions"
+                  onChange={(e) => setFilterText(e.target.value)}
+                />
+              </Column>
               <Column sm={1} md={1}>
                 <Toggle
                   id="devConfigSwitch"
@@ -146,7 +205,7 @@ export const Configuration: React.FC<ConfigurationProps> = () => {
         }}
       >
         <div className={styles.configTreePane}>
-          <ConfigTree config={config} />
+          <ConfigTree config={filteredConfig} />
         </div>
         <div className={styles.descriptionPane}>
           <Description />
