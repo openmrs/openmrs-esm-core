20b4bad0ccef3ee846879498476fc5b0e3b153fc
cynthia chebet <chebetcynthia8@gmail.com>
2021-08-19 10:20:14 +0300

    add ability to close app menu and use name when user clicks outside

--- packages/apps/esm-primary-navigation-app/src/components/navbar-header-panels/app-menu-panel.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/navbar-header-panels/app-menu-panel.component.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ExtensionSlot } from "@openmrs/esm-framework";
 import { HeaderPanel } from "carbon-components-react/es/components/UIShell";
 import styles from "./app-menu-panel.component.scss";
+import useOnClickOutside from "./useOnClickOutside";
 
 interface AppMenuProps {
   expanded: boolean;
+  hidePanel: () => void;
 }
 
-const AppMenuPanel: React.FC<AppMenuProps> = ({ expanded }) => {
+const AppMenuPanel: React.FC<AppMenuProps> = ({ expanded, hidePanel }) => {
+  const appMenuRef = useRef(null);
+
+  useOnClickOutside(appMenuRef, hidePanel, expanded);
   return (
     <HeaderPanel
+      ref={appMenuRef}
       className={styles.headerPanel}
       aria-label="App Menu Panel"
       expanded={expanded}
