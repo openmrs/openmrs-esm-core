openmrs/openmrs-esm-core

commit 83753e15b1bfd8c7fd2d5ed9df6b8c9894f5ec98

Use carbon-components-react as direct import

packages/apps/esm-implementer-tools-app/src/configuration/configuration.component.tsx
@@ -12,17 +12,17 @@ import {
   useExtensionStore,
   useStore,
 } from "@openmrs/esm-framework";
-import Button from "carbon-components-react/es/components/Button";
-import { Column, Grid, Row } from "carbon-components-react/es/components/Grid";
-import TextInput from "carbon-components-react/es/components/TextInput";
-import Toggle from "carbon-components-react/es/components/Toggle";
-import cloneDeep from "lodash-es/cloneDeep";
-import isEmpty from "lodash-es/isEmpty";
+import { Button } from "carbon-components-react";
+import { Column, Grid, Row } from "carbon-components-react";
+import { TextInput } from "carbon-components-react";
+import { Toggle } from "carbon-components-react";
 import { useTranslation } from "react-i18next";
 import { implementerToolsStore, ImplementerToolsStore } from "../store";
 import { ConfigTree } from "./config-tree.component";
-import styles from "./configuration.styles.css";
 import { Description } from "./description.component";
+import cloneDeep from "lodash-es/cloneDeep";
+import isEmpty from "lodash-es/isEmpty";
+import styles from "./configuration.styles.css";
 
 function isLeaf(configNode: Config) {
   return (

packages/apps/esm-implementer-tools-app/src/configuration/value-editors/array-editor.tsx
@@ -3,16 +3,16 @@ import cloneDeep from "lodash-es/cloneDeep";
 import uniqueId from "lodash-es/uniqueId";
 import Add16 from "@carbon/icons-react/es/add/16";
 import TrashCan16 from "@carbon/icons-react/es/trash-can/16";
-import Button from "carbon-components-react/es/components/Button";
 import styles from "./array-editor.styles.css";
+import { Button } from "carbon-components-react";
 import { Type } from "@openmrs/esm-framework";
-import { Tile } from "carbon-components-react/es/components/Tile";
+import { Tile } from "carbon-components-react";
 import {
   StructuredListBody,
   StructuredListCell,
   StructuredListRow,
   StructuredListWrapper,
-} from "carbon-components-react/es/components/StructuredList";
+} from "carbon-components-react";
 import { ValueEditorField } from "./value-editor-field";
 import { ConfigValueDescriptor } from "../editable-value.component";
 

packages/apps/esm-implementer-tools-app/src/configuration/value-editors/value-editor-field.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import uniqueId from "lodash-es/uniqueId";
-import Checkbox from "carbon-components-react/es/components/Checkbox";
-import NumberInput from "carbon-components-react/es/components/NumberInput";
-import TextInput from "carbon-components-react/es/components/TextInput";
+import { Checkbox } from "carbon-components-react";
+import { NumberInput } from "carbon-components-react";
+import { TextInput } from "carbon-components-react";
 import { Type } from "@openmrs/esm-framework";
 import { ConfigValueDescriptor } from "../editable-value.component";
 import { ValueType } from "../value-editor";

packages/apps/esm-implementer-tools-app/src/popup/popup.component.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Close16 from "@carbon/icons-react/es/close/16";
-import Button from "carbon-components-react/es/components/Button";
-import ContentSwitcher from "carbon-components-react/es/components/ContentSwitcher";
-import Switch from "carbon-components-react/es/components/Switch";
+import { Button } from "carbon-components-react";
+import { ContentSwitcher } from "carbon-components-react";
+import { Switch } from "carbon-components-react";
 import styles from "./popup.styles.css";
 import { useTranslation } from "react-i18next";
 import { Configuration } from "../configuration/configuration.component";

packages/apps/esm-implementer-tools-app/src/backend-dependencies/backend-dependencies.component.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import DataTable, {
+import {
+  DataTable,
   Table,
   TableHead,
   TableRow,
@@ -8,7 +9,7 @@ import DataTable, {
   TableBody,
   TableCell,
   TableContainer,
-} from "carbon-components-react/es/components/DataTable";
+} from "carbon-components-react";
 import { FrontendModule } from "./openmrs-backend-dependencies";
 
 export interface ModuleDiagnosticsProps {

packages/apps/esm-implementer-tools-app/src/configuration/config-tree.component.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from "./configuration.styles.css";
-import Accordion, {
-  AccordionItem,
-} from "carbon-components-react/es/components/Accordion";
+import { Accordion, AccordionItem } from "carbon-components-react";
 import { ConfigTreeForModule } from "./config-tree-for-module.component";
 
 export interface ConfigTreeProps {

packages/apps/esm-implementer-tools-app/src/configuration/editable-value.component.tsx
@@ -3,8 +3,8 @@ import isEqual from "lodash-es/isEqual";
 import unset from "lodash-es/unset";
 import cloneDeep from "lodash-es/cloneDeep";
 import Reset16 from "@carbon/icons-react/es/reset/16";
-import Button from "carbon-components-react/es/components/Button";
 import styles from "./editable-value.styles.css";
+import { Button } from "carbon-components-react";
 import {
   ConfigValue,
   Validator,

packages/apps/esm-implementer-tools-app/src/configuration/layout/subtree.component.tsx
@@ -4,7 +4,7 @@ import {
   StructuredListCell,
   StructuredListRow,
   StructuredListWrapper,
-} from "carbon-components-react/es/components/StructuredList";
+} from "carbon-components-react";
 import styles from "./layout.styles.css";
 
 export interface SubtreeProps {

commit 96b39957d78c675d54c8032a430e7a490917e34a

Fix eslint error in implementer-tools introduced by 83753e15b1bfd8c7fd2d5ed9df6b8c9894f5ec98

packages/apps/esm-implementer-tools-app/src/configuration/configuration.component.tsx
@@ -12,10 +12,14 @@ import {
   useExtensionStore,
   useStore,
 } from "@openmrs/esm-framework";
-import { Button } from "carbon-components-react";
-import { Column, Grid, Row } from "carbon-components-react";
-import { TextInput } from "carbon-components-react";
-import { Toggle } from "carbon-components-react";
+import {
+  Button,
+  Column,
+  Grid,
+  Row,
+  TextInput,
+  Toggle,
+} from "carbon-components-react";
 import { useTranslation } from "react-i18next";
 import { implementerToolsStore, ImplementerToolsStore } from "../store";
 import { ConfigTree } from "./config-tree.component";

packages/apps/esm-implementer-tools-app/src/configuration/value-editors/array-editor.tsx
@@ -4,10 +4,10 @@ import uniqueId from "lodash-es/uniqueId";
 import Add16 from "@carbon/icons-react/es/add/16";
 import TrashCan16 from "@carbon/icons-react/es/trash-can/16";
 import styles from "./array-editor.styles.css";
-import { Button } from "carbon-components-react";
 import { Type } from "@openmrs/esm-framework";
-import { Tile } from "carbon-components-react";
 import {
+  Button,
+  Tile,
   StructuredListBody,
   StructuredListCell,
   StructuredListRow,

packages/apps/esm-implementer-tools-app/src/configuration/value-editors/value-editor-field.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import uniqueId from "lodash-es/uniqueId";
-import { Checkbox } from "carbon-components-react";
-import { NumberInput } from "carbon-components-react";
-import { TextInput } from "carbon-components-react";
+import { Checkbox, NumberInput, TextInput } from "carbon-components-react";
 import { Type } from "@openmrs/esm-framework";
 import { ConfigValueDescriptor } from "../editable-value.component";
 import { ValueType } from "../value-editor";

packages/apps/esm-implementer-tools-app/src/popup/popup.component.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import Close16 from "@carbon/icons-react/es/close/16";
-import { Button } from "carbon-components-react";
-import { ContentSwitcher } from "carbon-components-react";
-import { Switch } from "carbon-components-react";
+import { Button, ContentSwitcher, Switch } from "carbon-components-react";
 import styles from "./popup.styles.css";
 import { useTranslation } from "react-i18next";
 import { Configuration } from "../configuration/configuration.component";