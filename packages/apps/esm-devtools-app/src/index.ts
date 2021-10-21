00f67cf551fbb590645d9fd085c0305fe96f24e8
Brandon Istenes <bistenes@gmail.com>
2021-10-21 14:13:04 -0700

    Make another test change

--- packages/apps/esm-devtools-app/src/index.ts
+++ packages/apps/esm-devtools-app/src/index.ts
@@ -24,7 +24,7 @@ function setupOpenMRS() {
 }
 
 function nothing() {
-  const thisIs = "a test change";
+  const thisIs = "another test change";
 }
 
 export { setupOpenMRS, importTranslation, frontendDependencies };
