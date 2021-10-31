f94a5d6e1ba0b155c3e50cae361984e2af5f718a
Brandon Istenes <bistenes@gmail.com>
2021-10-31 16:35:48 -0700

    Fix routes for 'openmrs develop'. The index.html route was failing. (#212)

--- packages/tooling/openmrs/src/commands/develop.ts
+++ packages/tooling/openmrs/src/commands/develop.ts
@@ -53,6 +53,22 @@ export function runDevelop(args: DevelopArgs) {
 
   const pageUrl = `http://${host}:${port}${spaPath}/`;
 
+  // Set up routes. Note that different middlewares have different rules
+  // about route precedence.
+  //
+  // HPM/createProxyMiddleware always takes top precedence, so we must
+  // explicitly exclude routes that we want to use other handlers for.
+  //
+  // express.static respects normal route declaration order.
+
+  // Return our custom `index.html` for all requests beginning with spaPath
+  // and not ending in `.js`, `.woff`, `.woff2`, `.json`, or any three-character
+  // extension.
+  const indexHtmlPathMatcher = new RegExp(
+    `^${spaPath}/(?!.*\\.js$)(?!.*\\.woff2?$)(?!.*\\.json$)(?!.*\\....$).*$`
+  );
+
+  // Route for custom `importmap.json` goes above static assets
   app.get(`${spaPath}/importmap.json`, (_, res) => {
     if (importmap.type === "inline") {
       res.contentType("application/json").send(importmap.value);
@@ -60,16 +76,32 @@ export function runDevelop(args: DevelopArgs) {
       res.redirect(importmap.value);
     }
   });
+
+  // Route for custom `index.html` goes above static assets
+  app.get(indexHtmlPathMatcher, (_, res) =>
+    res.contentType("text/html").send(indexContent)
+  );
+
+  // Return static assets for any request for which we have one, except importmap.json and index.html
+  app.use(spaPath, express.static(source, { index: false }));
+
+  // Proxy requests beginning with `apiUrl` but which should not serve `index.html`.
+  // This may include the JS bundles when using an import map that refers to
+  // JS bundles located at the same domain as `apiUrl`.
   app.use(
     apiUrl,
-    createProxyMiddleware([`${apiUrl}/**`, `!${spaPath}/**!(.js|.woff2?)`], {
-      target: backend,
-      changeOrigin: true,
-    })
-  );
-  app.use(spaPath, express.static(source, { index: false }));
-  app.get(`${spaPath}/*`, (_, res) =>
-    res.contentType("text/html").send(indexContent)
+    createProxyMiddleware(
+      (path) => {
+        return (
+          new RegExp(`${apiUrl}/.*`).test(path) &&
+          !indexHtmlPathMatcher.test(path)
+        );
+      },
+      {
+        target: backend,
+        changeOrigin: true,
+      }
+    )
   );
 
   app.listen(port, host, () => {
