0d9bf6802268c7e44e9900750c8497c343f000b0
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-11-24 16:01:52 +0100

    Renamed a file for consistency and added doc comments to a few components with design references.

--- packages/apps/esm-offline-tools-app/src/home/home.component.tsx
+++ packages/apps/esm-offline-tools-app/src/home/home.component.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import SharedPageLayout from "../components/shared-page-layout.component";
-import CardsContainer from "./cards-container";
+import CardsContainer from "./cards-container.component";
 import { ExtensionSlot } from "@openmrs/esm-framework";
 import styles from "./home.styles.scss";
 
+/**
+ * The offline tool's home/dashboard page.
+ * Renders the overview cards and provides an extension slot for the read-only offline patient table.
+ *
+ * Designs: https://zpl.io/an851Lr
+ */
 const Home: React.FC = () => {
   const { t } = useTranslation();
 
