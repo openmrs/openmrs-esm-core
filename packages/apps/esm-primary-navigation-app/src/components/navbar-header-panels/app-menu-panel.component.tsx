b071d9e10f68a8a6e32520fd92411e173f2d2ae7
Vineet Sharma <sharmava05@gmail.com>
2021-12-06 13:00:51 +0530

    O3-939: Added extension slot for adding external links in the app-menu (#226)
    * Added extension slot for external links in the app-menu
    
    * Removed the extension slot and added the code for fetching the links from the configuration.
    
    * Added visiblity option for external links
    
    * Added pre-push test back

--- packages/apps/esm-primary-navigation-app/src/components/navbar-header-panels/app-menu-panel.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/navbar-header-panels/app-menu-panel.component.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "./app-menu-panel.component.scss";
-import { ExtensionSlot, useOnClickOutside } from "@openmrs/esm-framework";
+import {
+  ExtensionSlot,
+  useOnClickOutside,
+  useConfig,
+} from "@openmrs/esm-framework";
 import { HeaderPanel } from "carbon-components-react";
+import Launch16 from "@carbon/icons-react/es/launch/16";
 
 interface AppMenuProps {
   expanded: boolean;
@@ -10,6 +15,7 @@ interface AppMenuProps {
 
 const AppMenuPanel: React.FC<AppMenuProps> = ({ expanded, hidePanel }) => {
   const appMenuRef = useOnClickOutside<HTMLDivElement>(hidePanel, expanded);
+  const config = useConfig();
 
   return (
     <HeaderPanel
@@ -22,6 +28,24 @@ const AppMenuPanel: React.FC<AppMenuProps> = ({ expanded, hidePanel }) => {
         className={styles.menuLink}
         extensionSlotName="app-menu-slot"
       />
+      {config?.externalRefLinks?.enabled &&
+        config?.externalRefLinks?.links.length > 0 && (
+          <div className={`${styles.menuLink} ${styles.externalLinks}`}>
+            {config?.externalRefLinks?.links?.map(
+              (link) =>
+                link?.visible && (
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={link?.redirect}
+                  >
+                    {link?.title}
+                    <Launch16 className={styles.launchIcon} />
+                  </a>
+                )
+            )}
+          </div>
+        )}
     </HeaderPanel>
   );
 };
--- packages/apps/esm-primary-navigation-app/src/config-schema.ts
+++ packages/apps/esm-primary-navigation-app/src/config-schema.ts
@@ -19,4 +19,37 @@ export const configSchema = {
       _description: "The organization name displayed when image is absent",
     },
   },
+  externalRefLinks: {
+    enabled: {
+      _type: Type.Boolean,
+      _default: true,
+      _description: "Whether to show external links in the app menu",
+    },
+    links: {
+      _type: Type.Array,
+      _elements: {
+        visible: {
+          _type: Type.Boolean,
+          _description: "Whether to show a link in the menu",
+          _default: true,
+        },
+        title: {
+          _type: Type.String,
+          _description: "Title of the link",
+        },
+        redirect: {
+          _type: Type.String,
+          _description: "Link to redirect to (must be an external link)",
+        },
+      },
+      _default: [
+        {
+          visible: false,
+          title: "External link",
+          redirect: "/external-link",
+        },
+      ],
+      _description: "The links to be showcased in the app menu",
+    },
+  },
 };
