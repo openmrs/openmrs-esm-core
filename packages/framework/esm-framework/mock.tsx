3d4db7bbb9576520d599147a94418ba8d6f887d5
Brandon Istenes <bistenes@gmail.com>
2022-01-06 19:27:25 -0800

    Expose date functions in mock; organize mock file

--- packages/framework/esm-framework/mock.tsx
+++ packages/framework/esm-framework/mock.tsx
@@ -2,8 +2,52 @@ import React from "react";
 import type {} from "@openmrs/esm-globals";
 import createStore, { Store } from "unistore";
 import { never, of } from "rxjs";
-import dayjs from "dayjs";
+export {
+  parseDate,
+  formatDate,
+  formatDatetime,
+  formatTime,
+} from "@openmrs/esm-utils";
 
+window.i18next = { ...window.i18next, language: "en" };
+
+/* esm-globals */
+
+export function setupPaths(config: any) {
+  window.openmrsBase = config.apiUrl;
+  window.spaBase = config.spaPath;
+  window.spaEnv = config.env || "production";
+  window.spaVersion = process.env.BUILD_VERSION;
+  window.getOpenmrsSpaBase = () => `${window.spaBase}/`;
+}
+
+/* esm-utils */
+export function isVersionSatisfied() {
+  return true;
+}
+
+/* esm-api */
+export const setSessionLocation = jest.fn(() => Promise.resolve());
+
+export const openmrsFetch = jest.fn(() => new Promise(() => {}));
+
+export const openmrsObservableFetch = jest.fn(() =>
+  of({ data: { entry: [] } })
+);
+
+export const getCurrentPatient = jest.fn(() =>
+  jest.fn().mockReturnValue(never())
+);
+
+export function getCurrentUser() {
+  return of({ authenticated: false });
+}
+
+export const newWorkspaceItem = jest.fn();
+
+export const fhirBaseUrl = "/ws/fhir2/R4";
+
+/* esm-state */
 interface StoreEntity {
   value: Store<any>;
   active: boolean;
@@ -14,12 +58,6 @@ export type MockedStore<T> = Store<T> & { resetMock: () => void };
 const initialStates: Record<string, any> = {};
 
 const availableStores: Record<string, StoreEntity> = {};
-
-export function isVersionSatisfied() {
-  return true;
-}
-
-export const setSessionLocation = jest.fn(() => Promise.resolve());
 
 export const mockStores = availableStores;
 
@@ -83,6 +121,7 @@ function instrumentedStore<T>(name: string, store: Store<T>) {
   } as any as MockedStore<T>;
 }
 
+/* esm-config */
 export const configInternalStore = createGlobalStore("config-internal", {
   devDefaultsAreOn: false,
 });
@@ -139,23 +178,51 @@ export function defineConfigSchema(moduleName, schema) {
   configSchema = schema;
 }
 
+export const navigate = jest.fn();
+
+export const interpolateString = jest.fn();
+
+export const ConfigurableLink = jest
+  .fn()
+  .mockImplementation((config: { to: string; children: React.ReactNode }) => (
+    <a href={interpolateString(config.to)}>{config.children}</a>
+  ));
+
+/* esm-error-handling */
 export const createErrorHandler = () => jest.fn().mockReturnValue(never());
 
 export const reportError = jest.fn().mockImplementation((error) => {
   throw error;
 });
 
+/* esm-extensions */
+
+export const attach = jest.fn();
+export const detach = jest.fn();
+export const detachAll = jest.fn();
+
 export const switchTo = jest.fn();
 
-export const UserHasAccessReact = (props: any) => props.children;
+export const ExtensionSlot = ({ children }) => <>{children}</>;
 
-export const openmrsFetch = jest.fn(() => new Promise(() => {}));
+export const Extension = jest.fn().mockImplementation((props: any) => <slot />);
 
-export const openmrsObservableFetch = jest.fn(() =>
-  of({ data: { entry: [] } })
-);
+export const getExtensionStore = () =>
+  getGlobalStore("extensions", { slots: {} });
 
-export const setIsUIEditorEnabled = (boolean): void => {};
+export const getExtensionInternalStore = () =>
+  getGlobalStore("extensions-internal", {
+    slots: {},
+    extensions: {},
+  });
+
+/* esm-react-utils */
+
+export const ComponentContext = React.createContext(null);
+
+export const openmrsComponentDecorator = jest
+  .fn()
+  .mockImplementation(() => (component) => component);
 
 export const useCurrentPatient = jest.fn(() => [null, null, null, null]);
 
@@ -170,49 +237,6 @@ export const useExtensionSlot = jest.fn(() => ({
 }));
 
 export const useExtension = jest.fn(() => [React.createRef(), undefined]);
-
-export const getCurrentPatient = jest.fn(() =>
-  jest.fn().mockReturnValue(never())
-);
-
-export function getCurrentUser() {
-  return of({ authenticated: false });
-}
-
-export const navigate = jest.fn();
-
-export const interpolateString = jest.fn();
-
-export const getCurrentPatientUuid = jest.fn();
-
-export const newWorkspaceItem = jest.fn();
-
-export const fhirBaseUrl = "/ws/fhir2/R4";
-
-export const ExtensionSlot = ({ children }) => <>{children}</>;
-
-export const Extension = jest.fn().mockImplementation((props: any) => <slot />);
-
-export const ConfigurableLink = jest
-  .fn()
-  .mockImplementation((config: { to: string; children: React.ReactNode }) => (
-    <a href={interpolateString(config.to)}>{config.children}</a>
-  ));
-
-export const getExtensionStore = () =>
-  getGlobalStore("extensions", { slots: {} });
-
-export const getExtensionInternalStore = () =>
-  getGlobalStore("extensions-internal", {
-    slots: {},
-    extensions: {},
-  });
-
-export const ComponentContext = React.createContext(null);
-
-export const openmrsComponentDecorator = jest
-  .fn()
-  .mockImplementation(() => (component) => component);
 
 export const UserHasAccess = jest.fn().mockImplementation((props: any) => {
   return props.children;
@@ -234,21 +258,6 @@ export const useStore = (store: Store<any>, actions) => {
   return { ...state, ...actions };
 };
 
-export const showNotification = jest.fn();
-export const showToast = jest.fn();
-
-export function setupPaths(config: any) {
-  window.openmrsBase = config.apiUrl;
-  window.spaBase = config.spaPath;
-  window.spaEnv = config.env || "production";
-  window.spaVersion = process.env.BUILD_VERSION;
-  window.getOpenmrsSpaBase = () => `${window.spaBase}/`;
-}
-
-export const attach = jest.fn();
-export const detach = jest.fn();
-export const detachAll = jest.fn();
-
 export const usePagination = jest.fn().mockImplementation(() => ({
   currentPage: 1,
   goTo: () => {},
@@ -257,27 +266,7 @@ export const usePagination = jest.fn().mockImplementation(() => ({
 
 export const useVisitTypes = jest.fn(() => []);
 
-export const formatDate = jest.fn((date: Date, mode) => {
-  if (!mode || mode == "standard") {
-    return dayjs(date).format("DD-MMM-YYYY");
-  }
-  if (mode == "wide") {
-    return dayjs(date).format("DD - MMM - YYYY");
-  }
-  if (mode == "no day") {
-    return dayjs(date).format("MMM YYYY");
-  }
-  if (mode == "no year") {
-    return dayjs(date).format("DD MMM");
-  }
-  console.warn("Unknown formatDate mode: ", mode);
-  return dayjs(date).format("DD-MMM-YYYY");
-});
+/* esm-styleguide */
 
-export const formatTime = jest.fn((date: Date) => {
-  return dayjs(date).format("HH:mm");
-});
-
-export const formatDatetime = jest.fn((date: Date, mode) => {
-  return formatDate(date, mode) + " " + formatTime(date);
-});
+export const showNotification = jest.fn();
+export const showToast = jest.fn();
