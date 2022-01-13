1913f23204e5ee527e449016ae820f661b0911fb
Florian Rappl <florian.rappl@smapiot.com>
2022-01-13 17:27:13 +0100

    Specify sub-path

--- tools/size-reporter.mjs
+++ tools/size-reporter.mjs
@@ -7,5 +7,5 @@ await reportFileSizeImpact({
   ...readGitHubWorkflowEnv(),
   buildCommand: "npx lerna run build",
   installCommand: "npx lerna bootstrap",
-  fileSizeReportModulePath: "./size-generator.mjs#fileSizeReport",
+  fileSizeReportModulePath: "./tools/size-generator.mjs#fileSizeReport",
 });
