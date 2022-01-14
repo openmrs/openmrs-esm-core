f2f02324535427c9db66fdb64495b3e990e98e5e
Florian Rappl <florian.rappl@smapiot.com>
2022-01-14 17:09:46 +0100

    Improved display

--- tools/size-generator.mjs
+++ tools/size-generator.mjs
@@ -1,26 +1,46 @@
 import { generateFileSizeReport } from "@jsenv/file-size-impact";
+import { promises } from "node:fs";
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const packagesDir = resolve(currentDir, "..", "packages");
+const manifestConfig = {};
+const trackingConfig = {};
+
+async function setPackages(subDir) {
+  const dir = resolve(packagesDir, subDir);
+  const names = await promises.readdir(dir);
+
+  await Promise.all(
+    names.map(async (name) => {
+      const p = resolve(dir, name);
+      const stat = await promises.stat(p);
+
+      if (stat.isDirectory()) {
+        const pj = resolve(p, "package.json");
+        const content = await promises.readFile(pj, "utf8");
+        const packageName = JSON.parse(content).name;
+        manifestConfig[
+          `./packages/${subDir}/${name}/dist/*.buildmanifest.json`
+        ] = true;
+        trackingConfig[packageName] = {
+          [`./packages/${subDir}/${name}/dist/*.js`]: true,
+          [`./packages/${subDir}/${name}/dist/*.css`]: true,
+          [`./packages/${subDir}/${name}/dist/*.map`]: false,
+          [`./packages/${subDir}/${name}/dist/*.json`]: false,
+        };
+      }
+    })
+  );
+}
+
+await setPackages("apps");
+await setPackages("shell");
 
 export const fileSizeReport = await generateFileSizeReport({
   log: process.argv.includes("--log"),
   projectDirectoryUrl: new URL("../", import.meta.url),
-  manifestConfig: {
-    "./packages/apps/*/dist/*.buildmanifest.json": true,
-    "./packages/shell/*/dist/*.buildmanifest.json": true,
-  },
-  trackingConfig: {
-    // all modules
-    modules: {
-      "./packages/apps/*/dist/*.js": true,
-      "./packages/apps/*/dist/*.css": true,
-      "./packages/apps/*/dist/*.map": false,
-      "./packages/apps/*/dist/*.json": false,
-    },
-    // all apps (should be only app-shell)
-    apps: {
-      "./packages/shell/*/dist/*.js": true,
-      "./packages/shell/*/dist/*.css": true,
-      "./packages/shell/*/dist/*.map": false,
-      "./packages/shell/*/dist/*.json": false,
-    },
-  },
+  manifestConfig,
+  trackingConfig,
 });
