75789b0b8d7eb8e28a2ea8467628e26131488ee5
Brandon Istenes <bistenes@gmail.com>
2022-01-18 20:03:27 -0800

    Make brand color configurable

--- packages/framework/esm-styleguide/src/index.ts
+++ packages/framework/esm-styleguide/src/index.ts
@@ -1,5 +1,6 @@
 import { setupLogo } from "./logo";
 import { setupIcons } from "./icons";
+import { defineConfigSchema, getConfigStore, Type } from "@openmrs/esm-config";
 
 export * from "./breakpoints";
 export * from "./spinner";
@@ -9,3 +10,20 @@ export * from "./modals";
 
 setupLogo();
 setupIcons();
+
+defineConfigSchema("@openmrs/esm-styleguide", {
+  "Primary brand color": {
+    _default: "#005d5d",
+    _type: Type.String,
+  },
+});
+
+getConfigStore("@openmrs/esm-styleguide").subscribe((store) => {
+  if (store.loaded && store.config) {
+    setGlobalCSSVariable("--brand-01", store.config["Primary brand color"]);
+  }
+});
+
+function setGlobalCSSVariable(name: string, value: string) {
+  document.documentElement.style.setProperty(name, value);
+}
