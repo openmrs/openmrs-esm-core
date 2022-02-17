68c2a1d94632cbcf980c855b7bfaeab3663842e9
Brandon Istenes <bistenes@gmail.com>
2022-02-16 17:24:00 -0800

    Fixup

--- packages/framework/esm-utils/src/translate.ts
+++ packages/framework/esm-utils/src/translate.ts
@@ -7,10 +7,9 @@ export function translateFrom(
   options?: object
 ) {
   const i18n: typeof _i18n = (_i18n as any).default || _i18n;
-  const tOptions = {
+  return i18n.t(key, {
     ns: moduleName,
     defaultValue: fallback,
     ...options,
-  };
-  return i18n.t(key, tOptions);
+  });
 }
