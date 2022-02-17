f308a215e1f665f9a7b1b8c3f3af9c501dc87c45
Brandon Istenes <bistenes@gmail.com>
2022-02-16 16:24:43 -0800

    (feat) Add options parameter to translateFrom

--- packages/framework/esm-utils/src/translate.ts
+++ packages/framework/esm-utils/src/translate.ts
@@ -3,11 +3,14 @@ import _i18n from "i18next";
 export function translateFrom(
   moduleName: string,
   key: string,
-  fallback?: string
+  fallback?: string,
+  options?: object
 ) {
   const i18n: typeof _i18n = (_i18n as any).default || _i18n;
-  return i18n.t(key, {
+  const tOptions = {
     ns: moduleName,
     defaultValue: fallback,
-  });
+    ...options,
+  };
+  return i18n.t(key, tOptions);
 }
