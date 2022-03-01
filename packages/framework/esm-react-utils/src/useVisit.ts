740bf2d1bc491a0e63418a763806f1f9de061212
Donald Kibet <chelashawdonald@yahoo.com>
2022-03-01 14:16:58 +0300

    03-1125: (bug) fix current visit retaining previous patient visit-data

--- packages/framework/esm-react-utils/src/useVisit.ts
+++ packages/framework/esm-react-utils/src/useVisit.ts
@@ -1,49 +1,43 @@
-import { useState, useEffect } from "react";
-import dayjs from "dayjs";
-import {
-  getVisitsForPatient,
-  getStartedVisit,
-  VisitMode,
-  VisitStatus,
-  Visit,
-} from "@openmrs/esm-api";
+import { openmrsFetch, Visit } from "@openmrs/esm-api";
+import useSWR from "swr";
 
-export function useVisit(patientUuid: string) {
-  const [currentVisit, setCurrentVisit] = useState<Visit | null>(null);
-  const [error, setError] = useState(null);
+interface VisitReturnType {
+  visits: Array<Visit>;
+  error: Error;
+  mutate: () => void;
+  isValidating: boolean;
+  currentVisit: Visit | null;
+}
 
-  useEffect(() => {
-    const sub = getStartedVisit.subscribe((visit) => {
-      if (visit) {
-        setCurrentVisit(visit?.visitData ?? null);
-      } else {
-        setCurrentVisit(null);
-      }
-    });
+const defaultVisitCustomRepresentation =
+  "custom:(uuid,encounters:(uuid,encounterDatetime," +
+  "form:(uuid,name),location:ref," +
+  "encounterType:ref,encounterProviders:(uuid,display," +
+  "provider:(uuid,display))),patient:(uuid,uuid)," +
+  "visitType:(uuid,name,display),attributes:(uuid,display,value),location:(uuid,name,display),startDatetime," +
+  "stopDatetime)";
+/**
+ * This React hook returns a visit object. If the `patientUuid` is provided
+ * as a parameter, then the visits, currentVisit, error and mutate function
+ * for that patient visit is returned.
+ * @param patientUuid Unique patient identifier `string`
+ * @param includeInactive Boolean that specifies whether to include Inactive visits `boolean`, defaults to false
+ * @returns Object {`visits`, `error` `isValidating`, `currentVisit`, `mutate`}
+ */
+export function useVisit(
+  patientUuid: string,
+  includeInactive: boolean = false
+): VisitReturnType {
+  const { data, error, mutate, isValidating } = useSWR<{
+    data: { results: Array<Visit> };
+  }>(
+    `/ws/rest/v1/visit?patient=${patientUuid}&v=${defaultVisitCustomRepresentation}&includeInactive=${includeInactive}`,
+    openmrsFetch
+  );
 
-    return () => sub.unsubscribe();
-  }, [patientUuid]);
+  const visits = data?.data.results ?? [];
+  const currentVisit =
+    data?.data.results.find((visit) => visit.stopDatetime === null) ?? null;
 
-  useEffect(() => {
-    const abortController = new AbortController();
-    const sub = getVisitsForPatient(patientUuid, abortController).subscribe(
-      ({ data }) => {
-        const currentVisit = data.results.find(
-          (visit) => visit.stopDatetime === null
-        );
-
-        if (currentVisit) {
-          getStartedVisit.next({
-            mode: VisitMode.LOADING,
-            visitData: currentVisit,
-            status: VisitStatus.ONGOING,
-          });
-        }
-      },
-      setError
-    );
-    return () => sub && sub.unsubscribe();
-  }, [patientUuid]);
-
-  return { currentVisit, error };
+  return { visits, error, mutate, isValidating, currentVisit };
 }
