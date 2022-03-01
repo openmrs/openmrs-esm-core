3195d77f6a91f75b75650483de5dd93a08df22ee
Donald Kibet <chelashawdonald@yahoo.com>
2022-03-01 22:09:39 +0300

    Code review changes

--- packages/framework/esm-react-utils/src/useVisit.ts
+++ packages/framework/esm-react-utils/src/useVisit.ts
@@ -1,43 +1,34 @@
-import { openmrsFetch, Visit } from "@openmrs/esm-api";
+import {
+  defaultVisitCustomRepresentation,
+  openmrsFetch,
+  Visit,
+} from "@openmrs/esm-api";
 import useSWR from "swr";
 
 interface VisitReturnType {
-  visits: Array<Visit>;
   error: Error;
   mutate: () => void;
   isValidating: boolean;
   currentVisit: Visit | null;
 }
 
-const defaultVisitCustomRepresentation =
-  "custom:(uuid,encounters:(uuid,encounterDatetime," +
-  "form:(uuid,name),location:ref," +
-  "encounterType:ref,encounterProviders:(uuid,display," +
-  "provider:(uuid,display))),patient:(uuid,uuid)," +
-  "visitType:(uuid,name,display),attributes:(uuid,display,value),location:(uuid,name,display),startDatetime," +
-  "stopDatetime)";
 /**
  * This React hook returns a visit object. If the `patientUuid` is provided
- * as a parameter, then the visits, currentVisit, error and mutate function
+ * as a parameter, then the currentVisit, error and mutate function
  * for that patient visit is returned.
  * @param patientUuid Unique patient identifier `string`
- * @param includeInactive Boolean that specifies whether to include Inactive visits `boolean`, defaults to false
- * @returns Object {`visits`, `error` `isValidating`, `currentVisit`, `mutate`}
+ * @returns Object {`error` `isValidating`, `currentVisit`, `mutate`}
  */
-export function useVisit(
-  patientUuid: string,
-  includeInactive: boolean = false
-): VisitReturnType {
+export function useVisit(patientUuid: string): VisitReturnType {
   const { data, error, mutate, isValidating } = useSWR<{
     data: { results: Array<Visit> };
   }>(
-    `/ws/rest/v1/visit?patient=${patientUuid}&v=${defaultVisitCustomRepresentation}&includeInactive=${includeInactive}`,
+    `/ws/rest/v1/visit?patient=${patientUuid}&v=${defaultVisitCustomRepresentation}&includeInactive=false`,
     openmrsFetch
   );
 
-  const visits = data?.data.results ?? [];
   const currentVisit =
     data?.data.results.find((visit) => visit.stopDatetime === null) ?? null;
 
-  return { visits, error, mutate, isValidating, currentVisit };
+  return { error, mutate, isValidating, currentVisit };
 }
