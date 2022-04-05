f73d1b42d1f94ec0c8e54175e0df578308496dd1
Brandon Istenes <bistenes@gmail.com>
2022-04-04 23:24:27 -0700

    Add missing dependencies to app shell

--- packages/shell/esm-app-shell/src/dependencies.ts
+++ packages/shell/esm-app-shell/src/dependencies.ts
@@ -5,10 +5,14 @@ import * as reactDom from "react-dom";
 import * as reactRouterDom from "react-router-dom";
 import * as reactI18next from "react-i18next";
 import * as singleSpa from "single-spa";
-import * as openmrsEsmFramework from "@openmrs/esm-framework";
+// Load @openmrs/esm-framework/src/internal because it is a superset
+// of @openmrs/esm-framework. There's no point loading both.
+import * as openmrsEsmFrameworkInternal from "@openmrs/esm-framework/src/internal";
 import * as carbonComponents from "carbon-components";
 import * as carbonIcons from "carbon-icons";
 import * as rxjs from "rxjs";
+
+(window as any).esmFrameworkInternal = openmrsEsmFrameworkInternal;
 
 // legacy way of dependency sharing - still done for backwards compatibility
 export const sharedDependencies = {
@@ -19,22 +23,23 @@ export const sharedDependencies = {
   "react-router-dom": () => reactRouterDom,
   "react-i18next": () => reactI18next,
   "single-spa": () => singleSpa,
-  "@openmrs/esm-api": () => openmrsEsmFramework,
+  "@openmrs/esm-api": () => openmrsEsmFrameworkInternal,
   "@openmrs/esm-app-shell": () => ({}),
-  "@openmrs/esm-breadcrumbs": () => openmrsEsmFramework,
-  "@openmrs/esm-config": () => openmrsEsmFramework,
-  "@openmrs/esm-module-config": () => openmrsEsmFramework,
-  "@openmrs/esm-context": () => openmrsEsmFramework,
-  "@openmrs/esm-error-handling": () => openmrsEsmFramework,
-  "@openmrs/esm-extensions": () => openmrsEsmFramework,
-  "@openmrs/esm-extension-manager": () => openmrsEsmFramework,
-  "@openmrs/esm-framework": () => openmrsEsmFramework,
-  "@openmrs/esm-globals": () => openmrsEsmFramework,
-  "@openmrs/esm-react-utils": () => openmrsEsmFramework,
-  "@openmrs/esm-state": () => openmrsEsmFramework,
-  "@openmrs/esm-styleguide": () => openmrsEsmFramework,
-  "@openmrs/esm-utils": () => openmrsEsmFramework,
-  "@openmrs/esm-offline": () => openmrsEsmFramework,
+  "@openmrs/esm-breadcrumbs": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-config": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-module-config": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-context": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-error-handling": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-extensions": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-extension-manager": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-framework": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-framework/src/internal": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-globals": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-react-utils": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-state": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-styleguide": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-utils": () => openmrsEsmFrameworkInternal,
+  "@openmrs/esm-offline": () => openmrsEsmFrameworkInternal,
   "carbon-components": () => carbonComponents,
   "carbon-icons": () => carbonIcons,
   rxjs: () => rxjs,
