cddfa5af53e4928d88163eea3a01d5486fcecadf
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-04-05 13:49:08 +0200

    Precache importmap refs as part of the static dependency lifecycle. Remove obsolete XMLHttpRequest patches.

--- packages/framework/esm-offline/src/index.ts
+++ packages/framework/esm-offline/src/index.ts
@@ -2,7 +2,6 @@ export * from "./service-worker";
 export * from "./service-worker-http-headers";
 export * from "./service-worker-messaging";
 export * from "./mode";
-export * from "./patches";
 export * from "./sync";
 export * from "./uuid";
 export * from "./offline-patient-data";
