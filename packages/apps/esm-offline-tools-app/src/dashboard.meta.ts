0ed0ee1ab24b7849476cf2325be7d3e2aedba60c
Brandon Istenes <bistenes@gmail.com>
2022-04-14 18:01:49 -0700

    Use name, not title

--- packages/apps/esm-offline-tools-app/src/dashboard.meta.ts
+++ packages/apps/esm-offline-tools-app/src/dashboard.meta.ts
@@ -1,5 +1,5 @@
 export const dashboardMeta = {
   slot: "patient-chart-offline-actions-dashboard-slot",
   config: { columns: 1, type: "grid" },
-  title: "Offline actions",
+  name: "Offline actions",
 };
