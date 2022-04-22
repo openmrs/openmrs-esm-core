13d3f064bd60d61b7dc5e054b0908e424357ce9d
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-04-23 01:04:30 +0530

    Make useLayout breakpoints consistent with CSS

--- packages/framework/esm-styleguide/src/breakpoints/index.ts
+++ packages/framework/esm-styleguide/src/breakpoints/index.ts
@@ -1,19 +1,19 @@
 function setBodyCssClasses() {
   document.body.classList.toggle(
     "omrs-breakpoint-lt-tablet",
-    window.innerWidth < 600
+    window.innerWidth <= 600
   );
   document.body.classList.toggle(
     "omrs-breakpoint-gt-phone",
-    window.innerWidth >= 600
+    window.innerWidth > 600
   );
   document.body.classList.toggle(
     "omrs-breakpoint-lt-desktop",
-    window.innerWidth < 1200
+    window.innerWidth <= 1200
   );
   document.body.classList.toggle(
     "omrs-breakpoint-gt-tablet",
-    window.innerWidth >= 1200
+    window.innerWidth > 1200
   );
 }
 
