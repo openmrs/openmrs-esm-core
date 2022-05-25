9a8aeadf643dc58ebe43f387a7371a88cdcd5192
Brandon Istenes <bistenes@gmail.com>
2022-05-25 09:21:35 -0700

    Bump version to 4.0 (#442)

--- packages/tooling/webpack-config/src/index.ts
+++ packages/tooling/webpack-config/src/index.ts
@@ -17,7 +17,7 @@ function getFrameworkVersion() {
     const { version } = require("@openmrs/esm-framework/package.json");
     return `^${version}`;
   } catch {
-    return "3.x";
+    return "4.x";
   }
 }
 
