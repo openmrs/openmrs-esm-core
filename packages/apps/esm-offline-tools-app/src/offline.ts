0f2304781d23bf04251455b78dd1e9a47f9c5442
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-06-03 13:19:53 +0200

    Fix offline actions not showing table. Improve patient sync handler.

--- packages/apps/esm-offline-tools-app/src/offline.ts
+++ packages/apps/esm-offline-tools-app/src/offline.ts
@@ -1,5 +1,7 @@
 import {
   fetchCurrentPatient,
+  makeUrl,
+  messageOmrsServiceWorker,
   setupDynamicOfflineDataHandler,
 } from "@openmrs/esm-framework";
 import { cacheForOfflineHeaders } from "./constants";
@@ -10,9 +12,20 @@ export function setupOffline() {
     displayName: "Offline tools",
     type: "patient",
     async isSynced(identifier) {
-      return true;
+      const expectedUrls = [`/ws/fhir2/R4/Patient/${identifier}`];
+      const absoluteExpectedUrls = expectedUrls.map(
+        (url) => window.origin + makeUrl(url)
+      );
+      const cache = await caches.open("omrs-spa-cache-v1");
+      const keys = (await cache.keys()).map((key) => key.url);
+      return absoluteExpectedUrls.every((url) => keys.includes(url));
     },
     async sync(identifier) {
+      await messageOmrsServiceWorker({
+        type: "registerDynamicRoute",
+        pattern: `/ws/fhir2/R4/Patient/${identifier}`,
+      });
+
       await fetchCurrentPatient(identifier, {
         headers: cacheForOfflineHeaders,
       });
