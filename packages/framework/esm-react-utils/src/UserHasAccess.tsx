bb61737cd467db9f5a1d4ad24fa2b6a5a09f04fb
Arjun-Go <arjun.g@thoughtworks.com>
2022-07-01 17:48:05 +0530

    add. enhance UserHasAccess component

--- packages/framework/esm-react-utils/src/UserHasAccess.tsx
+++ packages/framework/esm-react-utils/src/UserHasAccess.tsx
@@ -1,13 +1,18 @@
 /** @module @category API */
+import { getCurrentUser, LoggedInUser, userHasAccess } from "@openmrs/esm-api";
+import { InlineNotification } from "carbon-components-react";
 import React, { useEffect, useState } from "react";
-import { getCurrentUser, userHasAccess, LoggedInUser } from "@openmrs/esm-api";
 
 export interface UserHasAccessProps {
   privilege: string;
+  unauthorisedResponse?: string | undefined;
+  redirectUrl?: string | undefined;
 }
 
 export const UserHasAccess: React.FC<UserHasAccessProps> = ({
   privilege,
+  unauthorisedResponse,
+  redirectUrl,
   children,
 }) => {
   const [user, setUser] = useState<LoggedInUser | null>(null);
@@ -21,6 +26,22 @@ export const UserHasAccess: React.FC<UserHasAccessProps> = ({
 
   if (user && userHasAccess(privilege, user)) {
     return <>{children}</>;
+  }
+
+  if (unauthorisedResponse) {
+    return (
+      <div className="omrs-inline-notifications-container">
+        <InlineNotification
+          title="Unauthorised"
+          subtitle={unauthorisedResponse}
+          kind="error"
+        />
+      </div>
+    );
+  }
+
+  if (redirectUrl) {
+    window.location.replace(redirectUrl);
   }
 
   return null;
