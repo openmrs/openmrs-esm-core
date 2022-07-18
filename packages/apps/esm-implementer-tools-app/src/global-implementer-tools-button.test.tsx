fa3fe8f813d5fe282e523453255d59e4560805d9
Vineet Sharma <sharmava05@gmail.com>
2022-07-19 02:12:56 +0530

    Added global implementer tools button

---
+++ packages/apps/esm-implementer-tools-app/src/global-implementer-tools-button.test.tsx
@@ -0,0 +1,13 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom";
+import GlobalImplementerToolsButton from "./global-implementer-tools.component";
+
+describe("Testing the global implementer tools button", () => {
+  afterEach(cleanup);
+  it("should render global Implementer tools", () => {
+    render(<GlobalImplementerToolsButton />);
+    const button = screen.getByTestId("globalImplementerToolsButton");
+    expect(button).toBeInTheDocument();
+  });
+});
