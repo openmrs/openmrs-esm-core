b1dddf3cb14e9de179fbece31f81b9512bc7acea
Ian <52504170+ibacher@users.noreply.github.com>
2023-03-22 12:35:52 -0400

    (feat) Clear SWR cache on logout (#649)

--- packages/apps/esm-login-app/src/redirect-logout/logout.resource.ts
+++ packages/apps/esm-login-app/src/redirect-logout/logout.resource.ts
@@ -3,11 +3,17 @@ import {
   openmrsFetch,
   refetchCurrentUser,
 } from "@openmrs/esm-framework";
+import { mutate } from "swr";
 
 export async function performLogout() {
   await openmrsFetch("/ws/rest/v1/session", {
     method: "DELETE",
   });
+
+  // clear the SWR cache on logout, do not revalidate
+  // taken from the SWR docs
+  mutate(() => true, undefined, { revalidate: false });
+
   clearCurrentUser();
   await refetchCurrentUser();
 }
