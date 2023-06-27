421d0435b5f93b0921033637c124e5d7f1bf8e1d
Dennis Kigen <kigen.work@gmail.com>
2023-06-27 11:40:12 +0300

    (fix) Fix offline tools dashboard link

--- packages/apps/esm-offline-tools-app/src/createDashboardLink.tsx
+++ packages/apps/esm-offline-tools-app/src/createDashboardLink.tsx
@@ -1,15 +1,16 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import last from "lodash-es/last";
 import { BrowserRouter, useLocation } from "react-router-dom";
 import { ConfigurableLink } from "@openmrs/esm-framework";
-import styles from "./dashboard-link.scss";
-
 interface DashboardLinkConfig {
-  title: string;
+  basePath?: string;
+  path: string;
+  title: string | (() => string | Promise<string>);
 }
 
-const DashboardLink: React.FC<{ basePath: string; title: string }> = ({
+const DashboardLink: React.FC<DashboardLinkConfig> = ({
   basePath,
+  path,
   title,
 }) => {
   const location = useLocation();
@@ -17,26 +18,44 @@ const DashboardLink: React.FC<{ basePath: string; title: string }> = ({
     () => decodeURIComponent(last(location.pathname.split("/"))),
     [location.pathname]
   );
+  const [resolvedTitle, setResolvedTitle] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (typeof title === "function") {
+      Promise.resolve(title())
+        .then((resolvedValue) => {
+          setResolvedTitle(resolvedValue);
+        })
+        .catch((e: Error) => {
+          throw e;
+        });
+    } else {
+      setResolvedTitle(title);
+    }
+  }, [title]);
 
   const activeClassName =
-    title === navLink ? "active-left-nav-link" : "non-active";
+    path === navLink ? "active-left-nav-link" : "non-active";
 
   return (
-    <div key={title} className={activeClassName}>
-      <ConfigurableLink
-        to={`${basePath}/${encodeURIComponent(title)}`}
-        className={"cds--side-nav__link " + styles.link}
-      >
-        {title}
-      </ConfigurableLink>
-    </div>
+    title &&
+    resolvedTitle && (
+      <div key={path} className={activeClassName}>
+        <ConfigurableLink
+          to={`${basePath}/${encodeURIComponent(path)}`}
+          className={`cds--side-nav__link ${activeClassName}`}
+        >
+          {resolvedTitle}
+        </ConfigurableLink>
+      </div>
+    )
   );
 };
 
 export const createDashboardLink = (db: DashboardLinkConfig) => {
   return ({ basePath }: { basePath: string }) => (
     <BrowserRouter>
-      <DashboardLink title={db.title} basePath={basePath} />
+      <DashboardLink basePath={basePath} path={db.path} title={db.title} />
     </BrowserRouter>
   );
 };
--- packages/apps/esm-offline-tools-app/src/dashboard.meta.ts
+++ packages/apps/esm-offline-tools-app/src/dashboard.meta.ts
@@ -1,5 +1,6 @@
 export const dashboardMeta = {
   slot: "patient-chart-offline-actions-dashboard-slot",
   columns: 1,
+  path: "Offline Actions",
   title: "Offline Actions",
 };
