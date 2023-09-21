619579e89069e0a093d05204491a8ba562ae111e
arodidev <arodi745@gmail.com>
2023-09-21 17:55:30 +0300

    Adding configuration to allow loading css loaders for react-html5-camera-photo

--- packages/tooling/webpack-config/src/index.ts
+++ packages/tooling/webpack-config/src/index.ts
@@ -314,3 +314,18 @@ export default (
   };
   return mergeWith(baseConfig, additionalConfig, mergeFunction);
 };
+
+const cfgCreator = require('openmrs/default-webpack-config');
+
+module.exports = (env, argv) => {
+  const config = cfgCreator(env, argv);
+  const cssLoaderMode = resourcePath => {
+    if (/.*react-html5-camera-photo\/build\/css\/index.css/i.test(resourcePath) || /styles.css$/i.test(resourcePath)) {
+      return 'global';
+    }
+    return 'local';
+  };
+  config.module.rules[1].use[1].options.modules['mode'] = cssLoaderMode;
+  config.module.rules[2].use[1].options.modules['mode'] = cssLoaderMode;
+  return config;
+};
