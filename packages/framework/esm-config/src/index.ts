9d4f980fe2fd507ed2b28e2526dd189cc17a779b
Brandon Istenes <bistenes@gmail.com>
2024-01-09 21:30:23 +0300

    Break the backwards compatibility of packages importing nav functions directly from esm-config

--- packages/framework/esm-config/src/index.ts
+++ packages/framework/esm-config/src/index.ts
@@ -3,17 +3,3 @@ export * from './module-config/state';
 export * from './validators/validator';
 export * from './validators/validators';
 export * from './types';
-import {
-  navigate as na,
-  interpolateString as is,
-  interpolateUrl as iu,
-  TemplateParams,
-  NavigateOptions,
-} from '@openmrs/esm-navigation';
-/** @deprecated Please import from esm-navigation (or esm-framework) instead of esm-config */
-const navigate = na;
-/** @deprecated Please import from esm-navigation (or esm-framework) instead of esm-config */
-const interpolateString = is;
-/** @deprecated Please import from esm-navigation (or esm-framework) instead of esm-config */
-const interpolateUrl = iu;
-export { navigate, interpolateString, interpolateUrl, TemplateParams, NavigateOptions };
