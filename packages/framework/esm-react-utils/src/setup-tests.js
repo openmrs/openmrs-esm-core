d5fd8d7bd43f38142fc468d9b220af3c49e994fa
Brandon Istenes <bistenes@gmail.com>
2024-01-09 19:06:39 +0300

    Test fix

--- packages/framework/esm-react-utils/src/setup-tests.js
+++ packages/framework/esm-react-utils/src/setup-tests.js
@@ -1,5 +1,21 @@
+import { jest } from '@jest/globals'
 import '@testing-library/jest-dom';
 
 global.window.openmrsBase = '/openmrs';
 global.window.spaBase = '/spa';
 global.window.getOpenmrsSpaBase = () => '/openmrs/spa/';
+
+jest.mock('@openmrs/esm-config', () => ({
+  // Exporting everything in config except the re-export of the navigation functions
+  // that exist in `esm-config/src/index.ts` for backward compatibility. When those
+  // are removed, this mock too can be removed.
+  ...jest.requireActual('@openmrs/esm-config/src/module-config/module-config'),
+  ...jest.requireActual('@openmrs/esm-config/src/module-config/state'),
+  ...jest.requireActual('@openmrs/esm-config/src/validators/validator'),
+  ...jest.requireActual('@openmrs/esm-config/src/validators/validators'),
+}));
+
+jest.mock('@openmrs/esm-navigation', () => ({
+  ...jest.requireActual('@openmrs/esm-navigation'),
+  navigate: jest.fn(),
+}));
