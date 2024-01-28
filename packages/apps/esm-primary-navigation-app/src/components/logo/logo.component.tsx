0b36db0573e94a36e0962ef6ceced75bbc3aa7c6
suubi-joshua <ssebaanajoshua@gmail.com>
2024-01-28 21:32:22 +0300

    Added logic to allow dynamic adjustment of Logo width

--- packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
@@ -2,17 +2,30 @@ import React from 'react';
 import { interpolateUrl, useConfig } from '@openmrs/esm-framework';
 import styles from './logo.scss';
 
-const Logo: React.FC = () => {
+interface LogoProps {
+  width?: number; // Optional width property for the logo
+}
+
+const Logo: React.FC<LogoProps> = ({ width = 110 }) => {
   const { logo } = useConfig();
+
+  //Maximum width for the logo
+  const maxWidth = 300;
 
   return (
     <>
       {logo?.src ? (
-        <img className={styles.logo} src={interpolateUrl(logo.src)} alt={logo.alt} width={110} height={40} />
+        <img
+          className={styles.logo}
+          src={interpolateUrl(logo.src)}
+          alt={logo.alt}
+          width={Math.min(logo.width || width, maxWidth)} // Adjusted width logic
+          height={40}
+        />
       ) : logo?.name ? (
         logo.name
       ) : (
-        <svg role="img" width={110} height={40}>
+        <svg role="img" width={width} height={40}>
           <use xlinkHref="#omrs-logo-white"></use>
         </svg>
       )}
