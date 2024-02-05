dc336d41f21d5841b3b17b577615917ff7447164
suubi-joshua <ssebaanajoshua@gmail.com>
2024-02-05 04:28:09 +0300

    I preloaded image to get dimensions such that user does not need to specify width

--- packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import { interpolateUrl, useConfig } from '@openmrs/esm-framework';
 import styles from './logo.scss';
 
-
 const Logo: React.FC = () => {
   const { logo } = useConfig();
-  const logoWidth = logo?.width || 110;
-  //Maximum width for the logo
-  const maxWidth = 300;
+  const height = 40; // Fixed Height of the logo
+  const maxWidth = 300; // Maximum width of the logo
+
+  const calculateWidth = () => {
+    if (logo?.src) {
+      // Preload image to get dimensions
+      const img = new Image();
+      img.src = interpolateUrl(logo.src);
+
+      // Calculate width based on aspect ratio and constraints
+      const aspectRatio = img.width / img.height;
+      const calculatedWidth = Math.min(maxWidth, height * aspectRatio);
+
+      return calculatedWidth;
+    }
+    return 110; // If no logo is provided, use a default width 110
+  };
+
+
 
   return (
     <>
@@ -16,13 +31,13 @@ const Logo: React.FC = () => {
           className={styles.logo}
           src={interpolateUrl(logo.src)}
           alt={logo.alt}
-          width={Math.min( logoWidth, maxWidth)} // Adjusted width logic
+          width={calculateWidth()}
           height={40}
         />
       ) : logo?.name ? (
         logo.name
       ) : (
-        <svg role="img" width={logoWidth} height={40}>
+        <svg role="img" width={calculateWidth()} height={height}>
           <use xlinkHref="#omrs-logo-white"></use>
         </svg>
       )}
