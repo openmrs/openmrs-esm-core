3d811ce6db31f61f40d09db57f707d409e2c35fa
suubi-joshua <ssebaanajoshua@gmail.com>
2024-02-06 23:20:00 +0300

    Updated logo.scss to have default width of 110 and maxwidth of 300

--- packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
@@ -4,24 +4,13 @@ import styles from './logo.scss';
 
 const Logo: React.FC = () => {
   const { logo } = useConfig();
-  const height = 40; // Fixed Height of the logo
   const maxWidth = 300; // Maximum width of the logo
 
-  const calculateWidth = () => {
-    if (logo?.src) {
-      // Preload image to get dimensions
+   // Preload image to get dimensions
       const img = new Image();
       img.src = interpolateUrl(logo.src);
 
-      // Calculate width based on aspect ratio and constraints
-      const aspectRatio = img.width / img.height;
-      const calculatedWidth = Math.min(maxWidth, height * aspectRatio);
-
-      return calculatedWidth;
-    }
-    return 110; // If no logo is provided, use a default width 110
-  };
-
+    const logoWidth = img.width || 110; //default width 
 
 
   return (
@@ -31,13 +20,13 @@ const Logo: React.FC = () => {
           className={styles.logo}
           src={interpolateUrl(logo.src)}
           alt={logo.alt}
-          width={calculateWidth()}
+          width={logoWidth}
           height={40}
         />
       ) : logo?.name ? (
         logo.name
       ) : (
-        <svg role="img" width={calculateWidth()} height={height}>
+        <svg role="img" width={logoWidth} height={40}>
           <use xlinkHref="#omrs-logo-white"></use>
         </svg>
       )}
