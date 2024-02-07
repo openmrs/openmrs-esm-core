01eadea7065ffce2e29a9080ba6c0ddbffd56f68
kdaud <dkakumirizii@gmail.com>
2024-02-07 11:34:13 +0300

    Break down steps when accessing home page

--- e2e/specs/logout.spec.ts
+++ e2e/specs/logout.spec.ts
@@ -5,17 +5,21 @@ import { LoginPage } from '../pages';
 test('Logout as Admin user', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
-  await test.step('When I navigate to the home page', async () => {
+  await test.step('When I go to Login page', async () => {
     await loginPage.goto();
+  });
+
+  await test.step('And I enter the login credentials', async () => {
     await loginPage.enterLoginCredentials();
-    await page.goto('home');
+  });
+
+  await test.step('Then I should be in the Home page', async () => {
     await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/home`);
   });
 
   await test.step('And I click the `User` button', async () => {
     await page.getByRole('button', { name: /user/i }).click();
     await expect(page.getByText(/super user/i)).toBeVisible();
-    await expect(page.getByRole('button', { name: /logout/i })).toBeVisible();
   });
 
   await test.step('And I click the `Logout` button', async () => {
@@ -24,6 +28,9 @@ test('Logout as Admin user', async ({ page }) => {
 
   await test.step('Then I should be redirected to the login page', async () => {
     await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/login`);
+  });
+
+  await test.step('And I should not see any error messages', async () => {
     await expect(page.getByText('error')).not.toBeVisible();
   });
 });
