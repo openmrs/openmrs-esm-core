d208475b60982b368a9ee6f7f7257b36bd8a194b
suubi-joshua <ssebaanajoshua@gmail.com>
2024-02-08 14:29:24 +0300

    Width auto adjusted in css file

--- packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
@@ -14,6 +14,7 @@ const Logo: React.FC = () => {
           className={styles.logo}
           src={interpolateUrl(logo.src)}
           height={40}
+          alt={logo.alt}
         />
       ) : logo?.name ? (
         logo.name
