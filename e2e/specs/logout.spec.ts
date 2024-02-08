6166831fbcbc1235ece2c575149e806490849c32
kdaud <dkakumirizii@gmail.com>
2024-02-08 16:10:06 +0300

    O3-2826: Reuse signed in state in esm-core E2E tests excluding login test

--- e2e/specs/logout.spec.ts
+++ e2e/specs/logout.spec.ts
@@ -1,22 +1,7 @@
 import { test } from '../core';
 import { expect } from '@playwright/test';
-import { LoginPage } from '../pages';
 
-test('Logout as Admin user', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-
-  await test.step('When I go to Login page', async () => {
-    await loginPage.goto();
-  });
-
-  await test.step('And I enter the login credentials', async () => {
-    await loginPage.enterLoginCredentials();
-  });
-
-  await test.step('Then I should be on the Home page', async () => {
-    await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/home`);
-  });
-
+test('Logout as Admin user', async ({ page, api }) => {
   await test.step('When I click the `User` button', async () => {
     await page.getByRole('button', { name: /user/i }).click();
   });
