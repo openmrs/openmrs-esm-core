70a88abede77b9268ad004a767e8e9a0aab93779
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-02-13 14:35:37 +0300

    fix tests

--- packages/framework/esm-styleguide/src/snackbars/snackbar.test.tsx
+++ packages/framework/esm-styleguide/src/snackbars/snackbar.test.tsx
@@ -20,11 +20,11 @@ describe('Snackbar component', () => {
       closeSnackbar: closeSnackbarMock,
     };
 
-    const { getByText } = render(<Snackbar {...snackbarProps} />);
-    expect(getByText('Test Snackbar')).toBeInTheDocument();
+    render(<Snackbar {...snackbarProps} />);
+    expect(screen.getByText('Test Snackbar')).toBeInTheDocument();
   });
 
-  it('calls closeSnackbar when the close button is clicked', () => {
+  it('calls closeSnackbar when the close button is clicked', async () => {
     const snackbarProps = {
       snackbar: {
         id: 1,
@@ -33,10 +33,10 @@ describe('Snackbar component', () => {
       closeSnackbar: closeSnackbarMock,
     };
 
-    const { getByLabelText } = render(<Snackbar {...snackbarProps} />);
-    fireEvent.click(getByLabelText('Close snackbar'));
+    render(<Snackbar {...snackbarProps} />);
 
-    waitFor(() => expect(closeSnackbarMock).toHaveBeenCalled(), { timeout: 250 });
+    await user.click(screen.getByLabelText('Close snackbar'));
+    await waitFor(() => expect(closeSnackbarMock).toHaveBeenCalled());
   });
 
   it('calls onActionButtonClick when the action button is clicked', async () => {
@@ -45,7 +45,7 @@ describe('Snackbar component', () => {
       snackbar: {
         id: 1,
         title: 'Test Snackbar',
-        actionButtonLabel: 'undo',
+        actionButtonLabel: 'Undo',
         onActionButtonClick: onActionButtonClickMock,
       },
       closeSnackbar: closeSnackbarMock,
@@ -53,10 +53,8 @@ describe('Snackbar component', () => {
 
     render(<Snackbar {...snackbarProps} />);
 
-    await user.click(screen.getByText('undo'));
-    await waitFor(() => {
-      expect(onActionButtonClickMock).toHaveBeenCalled();
-    });
+    await user.click(screen.getByText('Undo'));
+    expect(onActionButtonClickMock).toHaveBeenCalled();
   });
 
   it('closes the snackbar after a timeout if autoClose is true', async () => {
@@ -73,9 +71,7 @@ describe('Snackbar component', () => {
 
     render(<Snackbar {...snackbarProps} />);
     jest.advanceTimersByTime(5000);
-    await waitFor(() => {
-      expect(closeSnackbarMock).toHaveBeenCalled();
-    });
+    await waitFor(() => expect(closeSnackbarMock).toHaveBeenCalled());
   });
 
   it('does not close the snackbar after a timeout if autoClose is false', async () => {
@@ -91,27 +87,6 @@ describe('Snackbar component', () => {
 
     render(<Snackbar {...snackbarProps} />);
     jest.advanceTimersByTime(5000);
-    await waitFor(() => {
-      expect(closeSnackbarMock).not.toHaveBeenCalled();
-    });
-  });
-
-  it('changes action text when action button is clicked', async () => {
-    const progressActionLabel = 'Progress Action';
-    const snackbarProps = {
-      snackbar: {
-        id: 1,
-        title: 'Test Snackbar',
-        actionButtonLabel: 'Undo',
-        progressActionLabel: progressActionLabel,
-      },
-      closeSnackbar: closeSnackbarMock,
-    };
-
-    render(<Snackbar {...snackbarProps} />);
-    await user.click(screen.getByText('Undo'));
-    await waitFor(() => {
-      expect(screen.getByText(progressActionLabel)).toBeInTheDocument();
-    });
+    expect(closeSnackbarMock).not.toHaveBeenCalled();
   });
 });
