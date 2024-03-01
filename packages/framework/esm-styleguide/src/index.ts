dae94737e6360b39b573ee541ac9251cfeb5fba4
Vineet Sharma <sharmava05@gmail.com>
2024-03-01 16:46:45 +0530

    Added new overlay component and 'showOverlay' API to display overlays

--- packages/framework/esm-styleguide/src/index.ts
+++ packages/framework/esm-styleguide/src/index.ts
@@ -10,9 +10,12 @@ export * from './notifications';
 export * from './toasts';
 export * from './snackbars';
 export * from './modals';
+export * from './overlays';
 export * from './left-nav';
 export * from './error-state';
 export * from './datepicker';
+
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 defineConfigSchema('@openmrs/esm-styleguide', esmStyleGuideSchema);
 setupBranding();
