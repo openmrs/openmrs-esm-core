a075b5d52b76fdf3a11091f0925a680fd0f912c6
Brandon Istenes <bistenes@gmail.com>
2024-04-18 22:29:13 -0400

    Add ActionMenuButton, adapted from SiderailButton

--- packages/framework/esm-styleguide/src/workspaces/index.ts
+++ packages/framework/esm-styleguide/src/workspaces/index.ts
@@ -1,4 +1,5 @@
 export * from './action-menu/action-menu.component';
+export * from './action-menu-button/action-menu-button.component';
 export * from './notification/workspace-notification.component';
 export * from './overlay/workspace-overlay.component';
 export * from './window/workspace-window.component';
--- packages/framework/esm-styleguide/src/workspaces/public.ts
+++ packages/framework/esm-styleguide/src/workspaces/public.ts
@@ -1,4 +1,5 @@
 export * from './action-menu/action-menu.component';
+export * from './action-menu-button/action-menu-button.component';
 export * from './overlay/workspace-overlay.component';
 export * from './window/workspace-window.component';
 export { type DefaultWorkspaceProps } from './types';
