cd9d412bc4a764d61b8aef879a9f2193fa3f9c4a
Brandon Istenes <bistenes@gmail.com>
2024-04-19 11:16:59 -0400

    Fix isChildOfHeader error

--- packages/apps/esm-primary-navigation-app/src/declarations.d.ts
+++ packages/apps/esm-primary-navigation-app/src/declarations.d.ts
@@ -2,4 +2,6 @@ declare module '*.css';
 declare module '*.scss';
 declare module '@carbon/react';
 declare type HeaderPanelProps = {};
-declare type SideNavProps = {};
+declare type SideNavProps = {
+  isChildOfHeader?: boolean;
+};
