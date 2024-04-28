a49360de2bcc72012f8149b4e16345c4a6e190da
Brandon Istenes <bistenes@gmail.com>
2024-04-28 19:02:49 -0400

    Go full send: move all the usual types into esm-framework

--- packages/apps/esm-primary-navigation-app/src/declarations.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.css';
-declare module '*.scss';
-declare module '@carbon/react';
-declare type HeaderPanelProps = {};
-declare type SideNavProps = {
-  isChildOfHeader?: boolean;
-};
--- packages/framework/esm-framework/src/declarations.d.ts
+++ packages/framework/esm-framework/src/declarations.d.ts
@@ -2,3 +2,10 @@ declare module '@carbon/react';
 declare type SideNavProps = {
   isChildOfHeader?: boolean;
 };
+declare module '*.css';
+declare module '*.scss';
+declare module '*.svg' {
+  const content: string;
+  export = content;
+}
+declare module '*.png';
