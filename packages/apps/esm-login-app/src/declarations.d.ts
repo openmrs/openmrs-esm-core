a5725bd59607e4c003cda602b18fc4f03e3d83b5
Brandon Istenes <bistenes@gmail.com>
2024-04-29 08:10:21 -0400

    (fix) Include common module type declarations in framework (#983)

--- packages/apps/esm-login-app/src/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-// This entire file will be deleted once we have typescript definitions
-// for @openmrs/esm-framework published to npm
-declare module '*.css';
-declare module '*.scss';
-declare module '@carbon/react';
-declare type SideNavProps = {};
