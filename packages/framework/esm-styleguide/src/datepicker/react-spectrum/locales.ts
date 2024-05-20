effc71112a86b3f9a28ae965054aa8c30d3eb003
kajambiya <jssebaggala@outlook.com>
2024-05-20 12:50:06 +0300

    Address review comments

--- packages/framework/esm-styleguide/src/datepicker/react-spectrum/locales.ts
+++ packages/framework/esm-styleguide/src/datepicker/react-spectrum/locales.ts
@@ -1,16 +1,5 @@
-import { type CalendarDate, EthiopicCalendar, toCalendar, IndianCalendar } from '@internationalized/date';
-
-const convertToEthiopicDate = (date: CalendarDate) => {
-  return toCalendar(date, new EthiopicCalendar());
-};
-
-const convertToIndianDate = (date: CalendarDate) => {
-  return toCalendar(date, new IndianCalendar());
-};
-
 export const supportedLocales = {
-  am: { locale: 'am-AM-u-ca-ethiopic', convert: convertToEthiopicDate },
-  am_ET: { locale: 'Amharic (Ethiopia)', convert: convertToEthiopicDate },
-  ti_ET: { locale: 'Tigrinya (Ethiopia)', convert: convertToEthiopicDate },
-  in: { locale: 'hi-IN-u-ca-indian', convert: convertToIndianDate },
+  am: 'Amharic',
+  am_ET: 'Amharic (Ethiopia)',
+  ti_ET: 'Tigrinya (Ethiopia)',
 };
