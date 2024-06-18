4e6446c43d1a638484b4cdf8ce7b055f3399e4d8
gitcliff <gitacliff48@gmail.com>
2024-06-18 16:28:54 +0300

    add password validation

--- packages/apps/esm-login-app/src/change-password/change-password.modal.tsx
+++ packages/apps/esm-login-app/src/change-password/change-password.modal.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Button, InlineLoading, ModalBody, ModalFooter, ModalHeader } from '@carbon/react';
 import styles from './change-password-modal.scss';
 import { Form, PasswordInput } from '@carbon/react';
+import { changeUserPassword } from './change-password.resource';
 
 interface ChangePasswordModalProps {
   close(): void;
@@ -14,10 +15,20 @@ export default function ChangeLanguageModal({ close }: ChangePasswordModalProps)
   const [oldPassword, setOldPassword] = useState<string>();
   const [newPassword, setNewPassword] = useState<string>();
   const [confirmPassword, setConfirmPassword] = useState<string>();
+  const [invalid, setInvalid] = useState(false);
+  const [invalidText, setInvalidText] = useState<string>();
 
   const handleSubmit = useCallback(() => {
     setIsChangingPassword(true);
-  }, []);
+    if (!newPassword && newPassword === confirmPassword) {
+      changeUserPassword(oldPassword, newPassword);
+    } else {
+      setIsChangingPassword(false);
+
+      setInvalid(true);
+      setInvalidText(t('passwordConfirmPasswordNotTheSame', 'New password and comfirm password are not the same'));
+    }
+  }, [oldPassword, newPassword, confirmPassword]);
 
   return (
     <>
@@ -32,10 +43,14 @@ export default function ChangeLanguageModal({ close }: ChangePasswordModalProps)
             <PasswordInput
               onChange={(event: ChangeEvent<HTMLInputElement>) => setNewPassword(event.target.value)}
               labelText={t('newPassword', 'New Password')}
+              invalid={invalid}
+              invalidText={invalidText}
             />
             <PasswordInput
               onChange={(event: ChangeEvent<HTMLInputElement>) => setConfirmPassword(event.target.value)}
               labelText={t('confirmPassword', 'Confirm New Password')}
+              invalid={invalid}
+              invalidText={invalidText}
             />
           </Form>
         </div>
