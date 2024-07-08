ceb80f068da8b29b2c816d6738dfa0d94f012702
Miiro Christopher <chrismiiroslav@gmail.com>
2024-07-08 18:09:38 +0300

    O3-3537-implement-a-change-password-page

--- packages/apps/esm-login-app/src/root.component.tsx
+++ packages/apps/esm-login-app/src/root.component.tsx
@@ -3,6 +3,7 @@ import Login from './login/login.component';
 import LocationPicker from './location-picker/location-picker.component';
 import RedirectLogout from './redirect-logout/redirect-logout.component';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ChangePassword from './change-password/change-password.component';
 
 export interface RootProps {}
 
@@ -14,6 +15,7 @@ const Root: React.FC<RootProps> = () => {
         <Route path="login/confirm" element={<Login />} />
         <Route path="login/location" element={<LocationPicker />} />
         <Route path="logout" element={<RedirectLogout />} />
+        <Route path="change-password" element={<ChangePassword />} />
       </Routes>
     </BrowserRouter>
   );
