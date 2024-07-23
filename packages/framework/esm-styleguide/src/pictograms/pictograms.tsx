60936ba847c4d2d0e8c4d28985cfaed96cb61577
Twiineenock <twiineenockfox@gmail.com>
2024-07-23 21:09:52 +0300

    registor page-header pictograms

--- packages/framework/esm-styleguide/src/pictograms/pictograms.tsx
+++ packages/framework/esm-styleguide/src/pictograms/pictograms.tsx
@@ -32,3 +32,33 @@ export const Pictogram = memo(
     );
   }),
 );
+
+export const HomePictogram = memo(
+  forwardRef<SVGSVGElement, PictogramProps>(function HomePictogram(props, ref) {
+    return <Pictogram ref={ref} pictogram="omrs-pict-home" pictogramProps={props} />;
+  }),
+);
+
+export const PatientListsPictogram = memo(
+  forwardRef<SVGSVGElement, PictogramProps>(function PatientListsPictogram(props, ref) {
+    return <Pictogram ref={ref} pictogram="omrs-pict-patient-lists" pictogramProps={props} />;
+  }),
+);
+
+export const AppointmentsPictogram = memo(
+  forwardRef<SVGSVGElement, PictogramProps>(function AppointmentsPictogram(props, ref) {
+    return <Pictogram ref={ref} pictogram="omrs-pict-appointments" pictogramProps={props} />;
+  }),
+);
+
+export const ServiceQueuesPictogram = memo(
+  forwardRef<SVGSVGElement, PictogramProps>(function ServiceQueuesPictogram(props, ref) {
+    return <Pictogram ref={ref} pictogram="omrs-pict-service-queues" pictogramProps={props} />;
+  }),
+);
+
+export const LaboratoryPictogram = memo(
+  forwardRef<SVGSVGElement, PictogramProps>(function LaboratoryPictogram(props, ref) {
+    return <Pictogram ref={ref} pictogram="omrs-pict-laboratory" pictogramProps={props} />;
+  }),
+);
