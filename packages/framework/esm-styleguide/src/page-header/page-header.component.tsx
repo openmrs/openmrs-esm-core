b7dbc2c8814730e865be103ecd34757494f9d1f1
Twiineenock <twiineenockfox@gmail.com>
2024-07-24 18:44:44 +0300

    fix a few issues

--- packages/framework/esm-styleguide/src/page-header/page-header.component.tsx
+++ packages/framework/esm-styleguide/src/page-header/page-header.component.tsx
@@ -6,23 +6,26 @@ import styles from './page-header.module.scss';
 import classNames from 'classnames';
 
 interface PageHeaderProps {
-  title: string;
+  title: string | JSX.Element;
   illustration: React.ReactElement;
+  className?: string;
 }
 interface PageHeaderContainerProps {
   children: React.ReactNode;
   className?: string;
 }
 
-export const PageHeaderContainer: React.FC<PageHeaderContainerProps> = ({ className, children }) => {
+export const PageHeaderContainer: React.FC<PageHeaderContainerProps> = ({ children, className }) => {
   return <div className={classNames(styles.pageHeaderContainer, className)}>{children}</div>;
 };
 
-export const PageHeader: React.FC<PageHeaderProps> = ({ title, illustration }) => {
-  const { clinicName } = useConfig<StyleguideConfigObject>();
+export const PageHeader: React.FC<PageHeaderProps> = ({ title, illustration, className }) => {
+  const { clinicName } = useConfig<StyleguideConfigObject>({
+    externalModuleName: '@openmrs/esm-styleguide',
+  });
 
   return (
-    <div className={styles.pageHeader}>
+    <div className={classNames(styles.pageHeader, className)}>
       {illustration}
       <div className={styles.pageLabels}>
         <p>{clinicName}</p>
