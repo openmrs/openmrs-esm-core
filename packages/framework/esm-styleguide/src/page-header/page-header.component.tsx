42652f66b4325d320687cd8691d6c055679bcfe4
Twiineenock <twiineenockfox@gmail.com>
2024-07-28 17:37:36 +0300

    Enable PageHeader imports with/without children

--- packages/framework/esm-styleguide/src/page-header/page-header.component.tsx
+++ packages/framework/esm-styleguide/src/page-header/page-header.component.tsx
@@ -5,32 +5,57 @@ import { type StyleguideConfigObject } from '../config-schema';
 import styles from './page-header.module.scss';
 import classNames from 'classnames';
 
-interface PageHeaderProps {
+interface PageHeaderContentProps {
   title: string | JSX.Element;
   illustration: React.ReactElement;
   className?: string;
 }
-interface PageHeaderContainerProps {
+
+interface PageHeaderProps {
   children: React.ReactNode;
   className?: string;
 }
 
-export const PageHeaderContainer: React.FC<PageHeaderContainerProps> = ({ children, className }) => {
-  return <div className={classNames(styles.pageHeaderContainer, className)}>{children}</div>;
+type ExcludeOptionalKeys<T, U> = {
+  [K in keyof T]: K extends keyof U ? never : K;
+}[keyof T];
+
+type XOR<T, U> =
+  | (T & { [K in ExcludeOptionalKeys<U, T>]?: never })
+  | (U & { [K in ExcludeOptionalKeys<T, U>]?: never });
+
+type ExactPageHeaderProps = XOR<PageHeaderProps, PageHeaderContentProps>;
+
+const isPageHeaderProps = (props: any): props is PageHeaderProps => {
+  return 'children' in props;
 };
 
-export const PageHeader: React.FC<PageHeaderProps> = ({ title, illustration, className }) => {
-  const { clinicName } = useConfig<StyleguideConfigObject>({
+export const PageHeaderContent: React.FC<PageHeaderContentProps> = ({ title, illustration, className }) => {
+  const { implementationName } = useConfig<StyleguideConfigObject>({
     externalModuleName: '@openmrs/esm-styleguide',
   });
 
   return (
-    <div className={classNames(styles.pageHeader, className)}>
+    <div className={classNames(styles.pageHeaderContent, className)}>
       {illustration}
       <div className={styles.pageLabels}>
-        <p>{clinicName}</p>
+        <p>{implementationName}</p>
         <p className={styles.pageName}>{title}</p>
       </div>
     </div>
   );
+};
+
+export const PageHeader: React.FC<ExactPageHeaderProps> = (props) => {
+  if (isPageHeaderProps(props)) {
+    const { children, className } = props;
+    return <div className={classNames(styles.pageHeader, className)}>{children}</div>;
+  } else {
+    const { title, illustration, className } = props;
+    return (
+      <div className={classNames(styles.pageHeader, className)}>
+        <PageHeaderContent title={title} illustration={illustration} />
+      </div>
+    );
+  }
 };
