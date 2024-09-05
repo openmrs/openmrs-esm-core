4c8a7a81ead1a5bd775d6fe1196585a9f92db233
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2024-09-05 15:52:16 +0300

    Use local shell to assemble and build E2E environment

--- e2e/support/github/run-e2e-docker-env.sh
+++ e2e/support/github/run-e2e-docker-env.sh
@@ -2,8 +2,10 @@
 
 # get the dir containing the script
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+repository_root="$script_dir/../../../."
 # create a temporary working directory
 working_dir=$(mktemp -d "${TMPDIR:-/tmp/}openmrs-e2e-frontends.XXXXXXXXXX")
+echo $working_dir
 # get a list of all the apps in this workspace
 apps=$(yarn workspaces list --json | jq -r 'if .location | test("-app$") then .name else empty end')
 # this array will hold all of the packed app names
@@ -20,7 +22,7 @@ do
   # run yarn pack for our app and add it to the working directory
   yarn workspace "$app" pack -o "$working_dir/$app_name.tgz" >/dev/null;
 done;
-echo "Created packed app archives" 
+echo "Created packed app archives"
 
 echo "Creating dynamic spa-assemble-config.json..."
 # dynamically assemble our list of frontend modules, prepending the
@@ -38,8 +40,21 @@ jq -n \
   )' | jq '{"frontendModules": .}' > "$working_dir/spa-assemble-config.json"
 echo "Created dynamic spa-assemble-config.json"
 
+echo "Copying tooling, shell and framework..."
+mkdir -p "$working_dir/packages"
+mkdir -p "$working_dir/packages/tooling"
+
+cp -r "$repository_root/.yarn" "$working_dir/.yarn"
+cp -r "$repository_root/.yarnrc.yml" "$working_dir/.yarnrc.yml"
+cp -r "$repository_root/packages/tooling" "$working_dir/packages"
+cp -r "$repository_root/packages/shell" "$working_dir/packages"
+cp -r "$repository_root/packages/framework" "$working_dir/packages"
+cp "$repository_root/package.json" "$working_dir/package.json"
+cp "$repository_root/yarn.lock" "$working_dir/yarn.lock"
+
 echo "Copying Docker configuration..."
 cp "$script_dir/Dockerfile" "$working_dir/Dockerfile"
+cp "$script_dir/.dockerignore" "$working_dir/.dockerignore"
 cp "$script_dir/docker-compose.yml" "$working_dir/docker-compose.yml"
 
 cd $working_dir
