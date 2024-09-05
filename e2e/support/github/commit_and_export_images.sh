3386c600dd242b746304d43e87aa5d9eb25ad68b
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2024-09-05 17:54:23 +0300

    Uncomment the export script

--- e2e/support/github/commit_and_export_images.sh
+++ e2e/support/github/commit_and_export_images.sh
@@ -6,12 +6,12 @@ frontend_container_id=$(docker ps --filter "name=frontend" --format "{{.ID}}")
 gateway_container_id=$(docker ps --filter "name=gateway" --format "{{.ID}}")
 
 docker commit $frontend_container_id frontend
-#docker commit $gateway_container_id gateway
-#docker commit $backend_container_id backend
-#docker commit $db_container_id db
+docker commit $gateway_container_id gateway
+docker commit $backend_container_id backend
+docker commit $db_container_id db
 
 docker save frontend > e2e_release_env_images.tar
-#docker save frontend gateway backend db > e2e_release_env_images.tar
+docker save frontend gateway backend db > e2e_release_env_images.tar
 
-# compress the file (to decrease the upload file size)
-#gzip -c e2e_release_env_images.tar > e2e_release_env_images.tar.gz
+ compress the file (to decrease the upload file size)
+gzip -c e2e_release_env_images.tar > e2e_release_env_images.tar.gz
