5d49bc07ca3bcf85bad6fd6390d7890c5d5e06c7
Samuel Muli <136758902+Samstar10@users.noreply.github.com>
2024-09-24 21:20:37 +0300

    (fix) O3-3891: Adjust Change Language Modal Size to Small (#1162)
    * (fix) O3-3891: Adjust Change Language Modal Size to Small
    
    * (fix) O3-3891: Adjust Change Language Modal Size to Small
    
    * (fix) O3-3891: Adjust Change Language Modal Size to Small
    
    * (fix) O3-3891: Adjust Change Language Modal Size to Small

---
+++ packages/apps/esm-primary-navigation-app/src/components/change-language/change-language-link.extension.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, SwitcherItem } from '@carbon/react';
+import { TranslateIcon, showModal, useSession } from '@openmrs/esm-framework';
+import styles from './change-language-link.scss';
+
+/** The user menu item that shows the current language and has a button to change the language */
+function ChangeLanguageLink() {
+  const { t } = useTranslation();
+  const session = useSession();
+
+  const launchChangeLanguageModal = useCallback(() => {
+    const dispose = showModal('change-language-modal', {
+      closeModal: () => dispose(),
+      size: 'sm',
+    });
+  }, []);
+
+  const languageNames = new Intl.DisplayNames([session?.locale], { type: 'language' });
+
+  return (
+    <SwitcherItem className={styles.panelItemContainer} aria-label={t('changeLanguage', 'Change language')}>
+      <div>
+        <TranslateIcon size={20} />
+        <p>{languageNames.of(session?.locale)}</p>
+      </div>
+      <Button kind="ghost" onClick={launchChangeLanguageModal}>
+        {t('change', 'Change')}
+      </Button>
+    </SwitcherItem>
+  );
+}
+
+export default ChangeLanguageLink;
