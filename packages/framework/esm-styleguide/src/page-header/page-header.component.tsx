5ee5890161c970da80984e257b4d9ce019824c7e
Vineet Sharma <sharmava05@gmail.com>
2024-12-13 23:58:05 +0530

    (fix) O3-4253: Support translation for the implementation name in the page header (#1232)
    * Support translation for the implementation name in the page header
    
    * Update failing tests

--- packages/framework/esm-styleguide/src/page-header/page-header.component.tsx
+++ packages/framework/esm-styleguide/src/page-header/page-header.component.tsx
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 import { getConfig } from '@openmrs/esm-config';
 import { type StyleguideConfigObject } from '../config-schema';
 import styles from './page-header.module.scss';
+import { type CoreTranslationKey, getCoreTranslation } from '@openmrs/esm-translations';
 
 export interface PageHeaderContentProps {
   title: string | JSX.Element;
@@ -99,7 +100,7 @@ export const PageHeaderContent: React.FC<PageHeaderContentProps> = ({ title, ill
     <div className={classNames(styles.pageHeaderContent, className)}>
       {illustration}
       <div className={styles.pageLabels}>
-        {config?.implementationName && <p>{config.implementationName}</p>}
+        {config?.implementationName && <p>{getCoreTranslation(config.implementationName as CoreTranslationKey)}</p>}
         <p className={styles.pageName}>{title}</p>
       </div>
     </div>
