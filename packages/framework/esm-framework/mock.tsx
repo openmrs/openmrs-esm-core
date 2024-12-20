aa9aa5a1991a9e02d4d022de905a2eb8b56327e7
Chi Bong Ho <cbho@pih.org>
2024-12-20 17:09:49 -0500

    (test) add mock for launchWorkspaceGroup

--- packages/framework/esm-framework/mock.tsx
+++ packages/framework/esm-framework/mock.tsx
@@ -91,6 +91,7 @@ export const ActionMenu = jest.fn(() => <div>Action Menu</div>);
 export const WorkspaceContainer = jest.fn(() => <div>Workspace Container</div>);
 export const closeWorkspace = jest.fn();
 export const launchWorkspace = jest.fn();
+export const launchWorkspaceGroup = jest.fn();
 export const navigateAndLaunchWorkspace = jest.fn();
 export const useWorkspaces = jest.fn();
 
