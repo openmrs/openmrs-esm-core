504c7fab9840911d6d40dbe29d4006c3e46f74f2
praneeth <141117659+Muppasanipraneeth@users.noreply.github.com>
2025-01-04 11:40:13 +0530

    rather than using capitalze from lodash-es done with function

--- packages/apps/esm-primary-navigation-app/src/components/change-language/change-language.modal.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/change-language/change-language.modal.tsx
@@ -12,7 +12,6 @@ import {
 import { useConnectivity, useSession } from '@openmrs/esm-framework';
 import { postUserPropertiesOffline, postUserPropertiesOnline } from './change-language.resource';
 import styles from './change-language.scss';
-import { capitalize } from 'lodash-es';
 
 interface ChangeLanguageModalProps {
   close(): void;
@@ -52,6 +51,9 @@ export default function ChangeLanguageModal({ close }: ChangeLanguageModalProps)
       ),
     [allowedLocales],
   );
+  const Capitalize = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
   return (
     <>
@@ -70,7 +72,7 @@ export default function ChangeLanguageModal({ close }: ChangeLanguageModalProps)
                 key={`locale-option-${locale}-${i}`}
                 id={`locale-option-${locale}-${i}`}
                 name={locale}
-                labelText={capitalize(languageNames[locale])}
+                labelText={Capitalize(languageNames[locale])}
                 value={locale}
               />
             ))}
