56af5f5dd99c3213640b8cb49b50464fa56bfd21
Dennis Kigen <kigen.work@gmail.com>
2025-05-29 13:03:44 +0300

    (feat) Additional content switcher style overrides
    - Adds a custom class to the ContentSwitcher in the popup
    - Removes outline and box-shadow from first and last child for a cleaner appearance

--- packages/apps/esm-implementer-tools-app/src/popup/popup.component.tsx
+++ packages/apps/esm-implementer-tools-app/src/popup/popup.component.tsx
@@ -48,6 +48,7 @@ export default function Popup({
       <div className={styles.topBar}>
         <div className={styles.tabs}>
           <ContentSwitcher
+            className={styles.contentSwitcher}
             selectedIndex={activeTab}
             onChange={(switcherItem: SwitcherItem) => {
               setActiveTab(switcherItem.index);
