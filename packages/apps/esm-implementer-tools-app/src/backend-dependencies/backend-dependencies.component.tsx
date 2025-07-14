9394431a8d8b003f326b2d9f18dfaeb4ed9c84c4
Harsh Thakkar <iharshthkkr@gmail.com>
2025-07-14 22:19:17 +0530

    (feat) Use theme component

--- packages/apps/esm-implementer-tools-app/src/backend-dependencies/backend-dependencies.component.tsx
+++ packages/apps/esm-implementer-tools-app/src/backend-dependencies/backend-dependencies.component.tsx
@@ -9,6 +9,7 @@ import {
   TableBody,
   TableCell,
   TableContainer,
+  Theme,
 } from '@carbon/react';
 import type { ResolvedDependenciesModule } from './openmrs-backend-dependencies';
 import styles from './backend-dependencies.styles.scss';
@@ -39,50 +40,52 @@ export const BackendDependencies: React.FC<BackendDependencies> = ({ backendDepe
   );
 
   return (
-    <div className={styles.container}>
-      <DataTable rows={[]} headers={headers}>
-        {({ headers, getTableProps, getHeaderProps }) => (
-          <TableContainer title="">
-            <Table {...getTableProps()}>
-              <TableHead>
-                <TableRow>
-                  {headers.map((header) => (
-                    <TableHeader {...getHeaderProps({ header })}>{header.header}</TableHeader>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {backendDependencies.map((esm) => (
-                  <Fragment key={esm.name}>
-                    <TableRow>
-                      <TableCell>
-                        <span className={styles.moduleHeader}>{esm.name}</span>
-                      </TableCell>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                    </TableRow>
-                    {esm.dependencies.map((dep) => (
-                      <TableRow key={dep.name}>
-                        <TableCell>{dep.name}</TableCell>
-                        <TableCell>
-                          {dep.type === 'missing' ? (
-                            <span style={{ color: 'red' }}>{t('missing', 'Missing')}</span>
-                          ) : dep.type === 'version-mismatch' ? (
-                            <span style={{ color: 'red' }}>{dep.installedVersion}</span>
-                          ) : (
-                            <span>{dep.installedVersion}</span>
-                          )}
-                        </TableCell>
-                        <TableCell>{dep.requiredVersion}</TableCell>
-                      </TableRow>
+    <Theme theme="g90">
+      <div className={styles.container}>
+        <DataTable rows={[]} headers={headers}>
+          {({ headers, getTableProps, getHeaderProps }) => (
+            <TableContainer title="">
+              <Table {...getTableProps()}>
+                <TableHead>
+                  <TableRow>
+                    {headers.map((header) => (
+                      <TableHeader {...getHeaderProps({ header })}>{header.header}</TableHeader>
                     ))}
-                  </Fragment>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-      </DataTable>
-    </div>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {backendDependencies.map((esm) => (
+                    <Fragment key={esm.name}>
+                      <TableRow>
+                        <TableCell>
+                          <span className={styles.moduleHeader}>{esm.name}</span>
+                        </TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                      </TableRow>
+                      {esm.dependencies.map((dep) => (
+                        <TableRow key={dep.name}>
+                          <TableCell>{dep.name}</TableCell>
+                          <TableCell>
+                            {dep.type === 'missing' ? (
+                              <span style={{ color: 'red' }}>{t('missing', 'Missing')}</span>
+                            ) : dep.type === 'version-mismatch' ? (
+                              <span style={{ color: 'red' }}>{dep.installedVersion}</span>
+                            ) : (
+                              <span>{dep.installedVersion}</span>
+                            )}
+                          </TableCell>
+                          <TableCell>{dep.requiredVersion}</TableCell>
+                        </TableRow>
+                      ))}
+                    </Fragment>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </DataTable>
+      </div>
+    </Theme>
   );
 };
