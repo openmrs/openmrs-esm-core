178f9dd251d149f8ba7de3dd629b3c2a976f51a4
Donald Kibet <chelashawdonald@yahoo.com>
2025-07-22 10:17:53 +0300

    additional logo

--- packages/apps/esm-login-app/src/footer.component.tsx
+++ packages/apps/esm-login-app/src/footer.component.tsx
@@ -23,36 +23,20 @@ const Footer: React.FC = () => {
     <div className={styles.footer}>
       <Tile className={styles.poweredByTile}>
         <div className={styles.poweredByContainer}>
-          <span className={styles.poweredByText}>{t('builtWith', 'Built with')}</span>
+          {logos.map((logo) => (
+            <img
+              alt={logo.alt ? t(logo.alt) : t('footerlogo', 'Footer Logo')}
+              className={styles.poweredByLogo}
+              key={logo.src}
+              onError={handleImageLoadError}
+              src={logo.src}
+            />
+          ))}
           <svg aria-label={t('openmrsLogo', 'OpenMRS Logo')} className={styles.poweredByLogo} role="img">
             <use href="#omrs-logo-full-color"></use>
           </svg>
-          <span className={styles.poweredByText}>
-            {t('poweredBySubtext', 'An open-source medical record system and global community')}
-          </span>
-          <Link
-            className={styles.learnMoreButton}
-            href="https://openmrs.org"
-            rel="noopener noreferrer"
-            renderIcon={() => <ArrowRightIcon size={16} aria-label="Arrow right icon" />}
-            target="_blank"
-          >
-            {t('learnMore', 'Learn more')}
-          </Link>
         </div>
       </Tile>
-
-      <div className={styles.logosContainer}>
-        {logos.map((logo) => (
-          <img
-            alt={logo.alt ? t(logo.alt) : t('footerlogo', 'Footer Logo')}
-            className={styles.poweredByLogo}
-            key={logo.src}
-            onError={handleImageLoadError}
-            src={logo.src}
-          />
-        ))}
-      </div>
     </div>
   );
 };
