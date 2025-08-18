e9715bd86f813c8963b6b616690559393a223a52
chibongho <cbho@pih.org>
2025-08-18 08:46:59 -0400

    (feat) O3-4869 Workspace v2 (#1424)
    * (feat) O3-4888 use common parcel loader for extensions / modals / workspaces
    
    * fix tests
    
    * fix build?
    
    * add way to specify loader function for extensions / modals / workspaces
    
    * address PR feedback
    
    * (feat) workspace v2
    
    * add option to close workspace without prompting for unsaved changes confirmation
    
    * Make workspace props optional; throw Errors (don't return them!)
    
    * (feat) O3-4927: add div to place single spa app roots in index.ejs
    
    * Don't use 'window' as a variable name in Javascript
    
    * add option to specify containerDomId for pages in routes.json; depcreate 'order' field
    
    * Ensure group props aren't set to null when launchWorkspace() is called with null group props
    
    * (feat) workspace overlay mode
    
    * CSS animation for sliding in / out workspaces
    
    * implement sliding animation for desktop mode
    
    * add mocks and minor styling
    
    * Apply suggestions from code review
    
    Co-authored-by: Brandon Istenes <bistenes@gmail.com>
    
    * address PR comment
    
    * *.md files
    
    ---------
    
    Co-authored-by: jwnasambu <33891016+jwnasambu@users.noreply.github.com>
    Co-authored-by: Brandon Istenes <bistenes@gmail.com>

---
+++ packages/framework/esm-framework/docs/functions/closeWorkspaceGroup2.md
@@ -0,0 +1,17 @@
+[O3 Framework](../API.md) / closeWorkspaceGroup2
+
+# Function: closeWorkspaceGroup2()
+
+> **closeWorkspaceGroup2**(): `Promise`\<`boolean`\>
+
+Defined in: [packages/framework/esm-styleguide/src/workspaces2/workspace2.ts:66](https://github.com/openmrs/openmrs-esm-core/blob/main/packages/framework/esm-styleguide/src/workspaces2/workspace2.ts#L66)
+
+Closes the workspace group that is currently opened. Note that only one workspace group
+may be opened at any given time
+
+## Returns
+
+`Promise`\<`boolean`\>
+
+a Promise that resolves to true if there is no opened group to begin with or we successfully closed
+the opened group; false otherwise.
--- packages/framework/esm-styleguide/src/index.ts
+++ packages/framework/esm-styleguide/src/index.ts
@@ -4,9 +4,21 @@ import { setupIcons } from './icons/icon-registration';
 import { setupBranding } from './brand';
 import { esmStyleGuideSchema } from './config-schema';
 import { setupPictograms } from './pictograms/pictogram-registration';
+import { registerModal } from '@openmrs/esm-extensions';
+import { getSyncLifecycle } from '@openmrs/esm-react-utils';
+import Workspace2ClosePromptModal from './workspaces2/workspace2-close-prompt.modal';
 
 defineConfigSchema('@openmrs/esm-styleguide', esmStyleGuideSchema);
 setupBranding();
 setupLogo();
 setupIcons();
 setupPictograms();
+
+registerModal({
+  name: 'workspace2-close-prompt',
+  moduleName: '@openmrs/esm-styleguide',
+  load: getSyncLifecycle(Workspace2ClosePromptModal, {
+    featureName: 'workspace2-close-prompt',
+    moduleName: '@openmrs/esm-styleguide',
+  }),
+});
