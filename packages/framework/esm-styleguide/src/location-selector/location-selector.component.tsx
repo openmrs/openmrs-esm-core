a3ae3a087b54bfa577edeb6fcd19f45fbb0f8c25
Bawanthathilan <bawantharathnayaka@gmail.com>
2025-10-06 09:57:59 +0530

    Add debounce for search term

--- packages/framework/esm-styleguide/src/location-selector/location-selector.component.tsx
+++ packages/framework/esm-styleguide/src/location-selector/location-selector.component.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ComboBox } from '@carbon/react';
 import { useLocationByUuid, useLocations } from '../location-picker/location-picker.resource';
+import { useDebounce } from '@openmrs/esm-react-utils';
 
 interface Location {
   resource: { id: string; name: string };
@@ -26,24 +27,24 @@ export const LocationSelector: React.FC<LocationSelectorProps> = ({
 }) => {
   const { t } = useTranslation();
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [tempLocations, setTempLocations] = useState<Location[]>([]);
-
+  const [initialLocations, setInitialLocations] = useState<Location[]>([]);
+  const debouncedSearchTerm = useDebounce(searchTerm);
   const { location: defaultLocation } = useLocationByUuid(defaultLocationUuid);
-  const { locations: fetchedLocations = [] } = useLocations(locationTag, locationsPerRequest, searchTerm);
+  const { locations: fetchedLocations = [] } = useLocations(locationTag, locationsPerRequest, debouncedSearchTerm);
 
   useEffect(() => {
     if (fetchedLocations.length > 0) {
-      setTempLocations(fetchedLocations);
+      setInitialLocations(fetchedLocations);
     }
   }, [fetchedLocations]);
 
   const effectiveLocations = useMemo<Location[]>(() => {
-    const base = fetchedLocations.length > 0 ? fetchedLocations : tempLocations;
+    const base = fetchedLocations.length > 0 ? fetchedLocations : initialLocations;
     if (defaultLocation && !searchTerm) {
       return [defaultLocation, ...base.filter((loc) => loc.resource.id !== defaultLocationUuid)];
     }
     return base;
-  }, [defaultLocation, defaultLocationUuid, fetchedLocations, tempLocations, searchTerm]);
+  }, [defaultLocation, defaultLocationUuid, fetchedLocations, initialLocations, searchTerm]);
 
   const items = useMemo(
     () =>
