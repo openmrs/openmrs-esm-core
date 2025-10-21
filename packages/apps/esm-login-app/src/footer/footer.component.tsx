5ae7dc7655e057237d797163b08519d2d618e25c
its-kios09 <legacyitsolution@gmail.com>
2025-10-21 19:08:43 +0300

    (refactor) Added responsiveness of the login page

--- packages/apps/esm-login-app/src/footer/footer.component.tsx
+++ packages/apps/esm-login-app/src/footer/footer.component.tsx
@@ -21,29 +21,43 @@ const Footer: React.FC = () => {
 
   return (
     <div className={styles.footer}>
-      <Tile className={styles.poweredByTile}>
-        <div className={styles.poweredByContainer}>
-          <div className={styles.logosContainer}>
-            {logos.map((logo, index) => (
-              <React.Fragment key={logo.src}>
-                <img
-                  alt={logo.alt ? t(logo.alt) : t('footerlogo', 'Footer Logo')}
-                  className={styles.poweredByLogo}
-                  onError={handleImageLoadError}
-                  src={logo.src}
-                />
-                {index < logos.length - 1 && <div className={styles.divider} />}
-              </React.Fragment>
-            ))}
+      {logos.length > 0 && (
+        <div className={styles.leftSection}>
+          <Tile className={styles.poweredByTile}>
+            <div className={styles.poweredByContainer}>
+              <div className={styles.logosContainer}>
+                {logos.map((logo, index) => (
+                  <React.Fragment key={logo.src}>
+                    <img
+                      alt={logo.alt ? t(logo.alt) : t('footerlogo', 'Footer Logo')}
+                      className={styles.poweredByLogo}
+                      onError={handleImageLoadError}
+                      src={logo.src}
+                    />
+                    {index < logos.length - 1 && <div className={styles.divider} />}
+                  </React.Fragment>
+                ))}
+              </div>
+            </div>
+          </Tile>
+        </div>
+      )}
 
-            {logos.length > 0 && <div className={styles.divider} />}
-
-            <svg aria-label={t('openmrsLogo', 'OpenMRS Logo')} className={styles.poweredByLogo} role="img">
-              <use href="#omrs-logo-full-color"></use>
+      <div className={styles.centerSection}>
+        <Tile className={styles.copyrightTile}>
+          <div className={styles.openmrsContainer}>
+            <svg
+              aria-label={t('openmrsLogo', 'OpenMRS Logo')}
+              className={styles.openmrsLogo}
+              role="img"
+              viewBox="0 0 200 40"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <use xlinkHref="#omrs-logo-full-color"></use>
             </svg>
           </div>
-        </div>
-      </Tile>
+        </Tile>
+      </div>
     </div>
   );
 };
