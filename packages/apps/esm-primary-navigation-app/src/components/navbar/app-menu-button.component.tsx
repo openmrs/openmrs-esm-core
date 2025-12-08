0167a9f36683f6d180c7f049e3a33e1deb5e4dd1
Abhee <abhaynimbalkar03@gmail.com>
2025-12-08 21:31:31 +0530

    (fix) O3-5258:  Fix app menu not closing with touch events

--- packages/apps/esm-primary-navigation-app/src/components/navbar/app-menu-button.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/navbar/app-menu-button.component.tsx
@@ -1,7 +1,7 @@
 import { HeaderGlobalAction } from '@carbon/react';
 import { CloseIcon, SwitcherIcon, useAssignedExtensions } from '@openmrs/esm-framework';
 import classNames from 'classnames';
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import AppMenuPanel from '../navbar-header-panels/app-menu-panel.component';
 import styles from './navbar.scss';
@@ -14,26 +14,39 @@ const AppMenuButton: React.FC<MenuButtonProps> = ({ isActivePanel, togglePanel, 
   const appMenuItems = useAssignedExtensions('app-menu-slot');
   const showAppMenu = useMemo(() => appMenuItems.length > 0, [appMenuItems.length]);
   const { t } = useTranslation();
+  const appMenuButtonRef = useRef<HTMLDivElement>(null);
+
+  const handleClose = useCallback(
+    (e: MouseEvent | TouchEvent) => {
+      // @ts-ignore
+      if (!appMenuButtonRef.current?.contains(e.target as Node)) {
+        hidePanel('appMenu')();
+      }
+    },
+    [hidePanel],
+  );
 
   return (
     showAppMenu && (
       <>
-        <HeaderGlobalAction
-          aria-label={t('AppMenuTooltip', 'App Menu')}
-          aria-labelledby="App Menu"
-          className={classNames({
-            [styles.headerGlobalBarButton]: isActivePanel('appMenu'),
-            [styles.activePanel]: !isActivePanel('appMenu'),
-          })}
-          isActive={isActivePanel('appMenu')}
-          onClick={() => {
-            togglePanel('appMenu');
-          }}
-          tooltipAlignment="end"
-        >
-          {isActivePanel('appMenu') ? <CloseIcon size={20} /> : <SwitcherIcon size={20} />}
-        </HeaderGlobalAction>
-        <AppMenuPanel expanded={isActivePanel('appMenu')} hidePanel={hidePanel('appMenu')} />
+        <div ref={appMenuButtonRef} style={{ display: 'contents' }}>
+          <HeaderGlobalAction
+            aria-label={t('AppMenuTooltip', 'App Menu')}
+            aria-labelledby="App Menu"
+            className={classNames({
+              [styles.headerGlobalBarButton]: isActivePanel('appMenu'),
+              [styles.activePanel]: !isActivePanel('appMenu'),
+            })}
+            isActive={isActivePanel('appMenu')}
+            onClick={() => {
+              togglePanel('appMenu');
+            }}
+            tooltipAlignment="end"
+          >
+            {isActivePanel('appMenu') ? <CloseIcon size={20} /> : <SwitcherIcon size={20} />}
+          </HeaderGlobalAction>
+        </div>
+        <AppMenuPanel expanded={isActivePanel('appMenu')} hidePanel={handleClose} />
       </>
     )
   );
