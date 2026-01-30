10d1caae1ce6150005384446f12eef4f0a60b3c0
Dennis Kigen <kigen.work@gmail.com>
2026-01-30 23:32:30 +0300

    Remove breadcrumbs extension registration

--- packages/shell/esm-app-shell/src/ui/index.ts
+++ packages/shell/esm-app-shell/src/ui/index.ts
@@ -1,13 +1,5 @@
 export const appName = '@openmrs/esm-app-shell';
 
 export function getCoreExtensions() {
-  return [
-    {
-      name: 'breadcrumbs-widget',
-      slot: 'breadcrumbs-slot',
-      component: 'breadcrumbs',
-      online: true,
-      offline: true,
-    },
-  ];
+  return [];
 }
