514515b2528ed2f3c809c1d460a4d0f947c5ac3c
Ian <52504170+ibacher@users.noreply.github.com>
2026-02-12 12:48:11 -0500

    (test) Move everything over to Vitest (#1591)

--- packages/apps/esm-primary-navigation-app/src/setupTests.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-const { getComputedStyle } = window;
-
-window.getComputedStyle = (elt) => getComputedStyle(elt);
-window.getOpenmrsSpaBase = jest.fn();
-window['getOpenmrsSpaBase'] = jest.fn().mockImplementation(() => '/');
