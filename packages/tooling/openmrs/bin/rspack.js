2419d61068852ebabfe3eb073f0880b011bfc101
Ian <ian.c.bacher@gmail.com>
2026-02-17 14:17:46 -0500

    Remove `main()` wrapper

--- packages/tooling/openmrs/bin/rspack.js
+++ packages/tooling/openmrs/bin/rspack.js
@@ -3,11 +3,7 @@
 // Re-export the rspack CLI so that packages depending on
 // openmrs get the `rspack` command without needing a direct
 // dependency on @rspack/cli.
-const { RspackCLI } = require("@rspack/cli");
+const { RspackCLI } = require('@rspack/cli');
 
-async function main() {
-  const cli = new RspackCLI();
-  await cli.run(process.argv);
-}
-
-main();
+const cli = new RspackCLI();
+cli.run(process.argv);
