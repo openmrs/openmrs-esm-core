dd859c3941248187d4ef736289dc65db399282da
dilankavishka <dkgamage01@gmail.com>
2026-07-23 11:12:26 +0530

    Add missing import

--- packages/apps/esm-login-app/src/two-factor-auth/two-factor-auth-link.extension.test.tsx
+++ packages/apps/esm-login-app/src/two-factor-auth/two-factor-auth-link.extension.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { vi, describe, beforeEach, it } from 'vitest';
+import { vi, describe, beforeEach, it, expect } from 'vitest';
 import { navigate } from '@openmrs/esm-framework';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
