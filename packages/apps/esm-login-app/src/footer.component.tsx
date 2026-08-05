3f9fbcae26a67125c055e7742168be64dbb35b94
Shrestha.Katiyar <shrestha.katiyar@beehyv.com>
2026-08-05 14:16:44 +0530

    Remove 'Built with OpenMRS' footer from login page

--- packages/apps/esm-login-app/src/footer.component.tsx
+++ packages/apps/esm-login-app/src/footer.component.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
-import { Link, Tile } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
-import { useConfig, ArrowRightIcon } from '@openmrs/esm-framework';
+import { useConfig } from '@openmrs/esm-framework';
 import { type ConfigSchema } from './config-schema';
 import styles from './footer.scss';
 
@@ -21,27 +20,6 @@ const Footer: React.FC = () => {
 
   return (
     <div className={styles.footer}>
-      <Tile className={styles.poweredByTile}>
-        <div className={styles.poweredByContainer}>
-          <span className={styles.poweredByText}>{t('builtWith', 'Built with')}</span>
-          <svg aria-label={t('openmrsLogo', 'OpenMRS Logo')} className={styles.poweredByLogo} role="img">
-            <use href="#omrs-logo-full-color"></use>
-          </svg>
-          <span className={`${styles.poweredByText} ${styles.poweredBySubtext}`}>
-            {t('poweredBySubtext', 'An open-source medical record system and global community')}
-          </span>
-          <Link
-            className={styles.learnMoreButton}
-            href="https://openmrs.org"
-            rel="noopener noreferrer"
-            renderIcon={() => <ArrowRightIcon size={16} aria-label="Arrow right icon" />}
-            target="_blank"
-          >
-            {t('learnMore', 'Learn more')}
-          </Link>
-        </div>
-      </Tile>
-
       <div className={styles.logosContainer}>
         {logos.map((logo) => (
           <img
